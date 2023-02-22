537d7d4f94e440f18117c88c6aeb2c6f2f09f2f6
ceo-py <sfmf@abv.bg>
2023-02-22 15:28:54 +0200

    init

---
+++ JS Syntax Fundamentals - Exercises/3_leap_year.js
@@ -0,0 +1,11 @@
+
+function solve(year) {
+    if (year % 400 === 0 || year % 4 ===0 && year % 100 !== 0){
+        console.log('yes')
+    } else {
+        console.log('no')
+    }
+}
+
+
+solve(4)
