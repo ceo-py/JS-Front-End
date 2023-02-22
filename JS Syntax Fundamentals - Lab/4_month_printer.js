fb23ed29ff5957a5e1120c739c9e74ec1b51a982
ceo-py <sfmf@abv.bg>
2023-02-22 13:01:17 +0200

    init

---
+++ JS Syntax Fundamentals - Lab/4_month_printer.js
@@ -0,0 +1,25 @@
+function solve(day) {
+    let calendar = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December',
+    }
+    if (day < 1 || day > 12) {
+        console.log('Error!')
+    }else {
+        console.log(calendar[day])
+    }
+
+}
+
+solve(12);
+
