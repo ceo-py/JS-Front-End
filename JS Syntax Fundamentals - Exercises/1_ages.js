337da38068a96e51b714f984e32c5b2c80f08cc8
ceo-py <sfmf@abv.bg>
2023-02-22 15:04:40 +0200

    init

---
+++ JS Syntax Fundamentals - Exercises/1_ages.js
@@ -0,0 +1,17 @@
+function solve(age) {
+    let output = 'out of bounds';
+    if (age >= 0 && age <= 2) {
+        output = 'baby';
+    } else if (age >= 3 && age <= 13) {
+        output = 'child';
+    } else if (age >= 14 && age <= 19) {
+        output = 'teenager';
+    } else if (age >= 20 && age <= 65) {
+        output = 'adult';
+    } else if (age >= 66) {
+        output = 'elder';
+    }
+    console.log(output)
+}
+
+solve(100)
