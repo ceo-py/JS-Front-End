0cf03af95c84c96f22f4eb5f51bc48ba018344e6
ceo-py <sfmf@abv.bg>
2023-02-22 20:37:45 +0200

    init

---
+++ JS Syntax Fundamentals - More Exercises/1_validity_checker.js
@@ -0,0 +1,28 @@
+function validateDistance(x1, y1, x2, y2) {
+    function distance(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    }
+
+    const distance1 = distance(x1, y1, 0, 0);
+    const distance2 = distance(x2, y2, 0, 0);
+    const distance3 = distance(x1, y1, x2, y2);
+
+    if (Number.isInteger(distance1)) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    } if (Number.isInteger(distance2)) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    } if (Number.isInteger(distance3)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+// example usage
+validateDistance(3, 0, 0, 4);
+// validateDistance(2, 1, 1, 1);
+
