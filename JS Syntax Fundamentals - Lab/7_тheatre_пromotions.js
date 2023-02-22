81cf017d7b7e34d67203feeb5ece216ed67c6dd8
ceo-py <sfmf@abv.bg>
2023-02-22 13:58:32 +0200

    init

---
+++ JS Syntax Fundamentals - Lab/7_тheatre_пromotions.js
@@ -0,0 +1,25 @@
+
+function solve (day, age) {
+    function ticketPrice (age, ticketMinPrice, ticketMidPrice, ticketMaxPrice) {
+    if (age >= 0 && age <= 18){
+            return  `${ticketMinPrice}$`;
+        }
+        else if (age > 18 && age <= 64){
+            return  `${ticketMidPrice}$`
+        }
+        else if (age > 64 && age <= 122){
+            return  `${ticketMaxPrice}$`
+        } else {
+            return "Error!"
+        }
+    }
+    if (day === 'Weekday') {
+        console.log(ticketPrice(age,  12, 18, 12))
+    } else if (day === 'Weekend') {
+        console.log(ticketPrice(age, 15, 20, 15))
+    } else if (day === 'Holiday') {
+        console.log(ticketPrice(age, 5, 12, 10))
+    }
+}
+
+solve('Weekday', 42)
