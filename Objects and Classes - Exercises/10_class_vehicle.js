edac04f8d67c12fa5f64273e6dc4a3710c189aa8
ceo-py <sfmf@abv.bg>
2023-02-23 20:03:25 +0200

    init

---
+++ Objects and Classes - Exercises/10_class_vehicle.js
@@ -0,0 +1,22 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type
+        this.model = model
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power,
+        }
+        this.fuel = fuel
+    }
+    drive(value) {
+        this.fuel -= value
+    }
+}
+
+
+let parts = {engine: 6, power: 100};
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
