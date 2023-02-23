3f175cc090ec603508ec3c748844ec72fbf6ae7d
ceo-py <sfmf@abv.bg>
2023-02-23 12:39:43 +0200

    init

---
+++ Objects and Classes - Lab/2_city.js
@@ -0,0 +1,21 @@
+
+function solve(dict){
+
+    // for (const key in dict) {
+    //     console.log(`${key}: ${dict[key]}`);
+    // }
+
+    for (const [key, value] of Object.entries(dict)){
+        console.log(`${key} -> ${value}`)
+
+    }
+}
+
+
+solve({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+})
