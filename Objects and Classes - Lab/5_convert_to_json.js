de1a0b11ebb54ec8ceb1611dd91953a283ba5edc
ceo-py <sfmf@abv.bg>
2023-02-23 13:00:04 +0200

    init

---
+++ Objects and Classes - Lab/5_convert_to_json.js
@@ -0,0 +1,12 @@
+
+function solve(...list){
+    const person = {
+        name: list.shift(),
+        lastName: list.shift(),
+        hairColor: list.shift()
+    }
+    console.log(JSON.stringify(person))
+}
+
+
+solve('George', 'Jones', 'Brown')
