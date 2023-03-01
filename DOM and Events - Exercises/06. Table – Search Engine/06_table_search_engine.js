d6b766c5d15e96bfb60eefb84cc0e43a8e711c1e
ceo-py <sfmf@abv.bg>
2023-03-01 14:48:54 +0200

    init

---
+++ DOM and Events - Exercises/06. Table – Search Engine/06_table_search_engine.js
@@ -0,0 +1,18 @@
+function solve() {
+
+    document.querySelector('#searchBtn').addEventListener('click', onClick)
+
+    function onClick() {
+        const table =[...document.querySelectorAll('tbody > tr')]
+        const searchField = document.querySelector('#searchField')
+
+        table.forEach(item => {
+            console.log(item.innerText)
+            item.classList.remove('select')
+            if (item.innerText.includes(searchField.value)&& searchField.value.length > 0) {
+                item.className = 'select'
+            }
+        })
+        searchField.value = ''
+    }
+}
