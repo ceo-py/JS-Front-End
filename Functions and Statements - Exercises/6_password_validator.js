6d28b9d50350d382a3522375902ae67e20d6912c
ceo-py <sfmf@abv.bg>
2023-03-08 13:14:55 +0200

    init

--- Functions and Statements - Exercises/6_password_validator.js
+++ Functions and Statements - Exercises/6_password_validator.js
@@ -18,3 +18,45 @@ function solve(password) {
 }
 
 solve('Pa$s$s')
+
+
+
+
+//
+// function solve(password) {
+//     let output = [];
+//
+//     function checkLetterAndNumbers(text) {
+//         let [valid, numbers] = [true, 0]
+//         for (let i = 0; i < text.length; i++) {
+//             let symbol = text.charCodeAt(i);
+//             if (!(symbol >= 48 && symbol <= 57) && !(symbol >= 65 && symbol <= 90) && !(symbol >= 97 && symbol <= 122)) {
+//                 valid = false;
+//             }
+//             if (symbol >= 48 && symbol <= 57) {
+//                 numbers++;
+//             }
+//         }
+//
+//         return [valid, numbers];
+//
+//     }
+//
+//     let [valid, numbers] = checkLetterAndNumbers(password)
+//
+//     if (password.length <= 5 || password.length > 10) {
+//         output.push("Password must be between 6 and 10 characters")
+//     }
+//     if (!valid) {
+//         output.push("Password must consist only of letters and digits")
+//     }
+//     if (numbers < 2) {
+//         output.push("Password must have at least 2 digits")
+//     }
+//     if (output.length > 0) {
+//         console.log(`${output.join('\n')}`)
+//     } else {
+//         console.log('Password is valid')
+//     }
+// }
+//
