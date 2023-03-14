fd3629e4c6575ccd6d3493632c030fdc70d4b236
ceo-py <sfmf@abv.bg>
2023-03-14 22:33:08 +0200

    init

--- JS-Front-End-Exams/Exam Preparation II/02. Music Site/app.js
+++ JS-Front-End-Exams/Exam Preparation II/02. Music Site/app.js
@@ -4,10 +4,9 @@ function solve() {
     const inputElements = Array.from(document.querySelectorAll('input[type="text"]'))
     const addBtn = document.querySelector('#add-btn')
     const allHits = document.querySelector('.all-hits-container')
-    let likes = document.querySelector('.likes > p')
-
 
     function correctInput() {
+
         for (const item of inputElements) {
             if (item.value.trim().length === 0) {
                 return false
@@ -35,22 +34,38 @@ function solve() {
     }
 
     function createButtonWithClassAndTextContent(tag, textContent, className) {
-        const b = createElementWithTextContent(tag, textContent)
-        b.classList.add(className)
+        const b = createElementWithClass(tag, className)
+        b.textContent = textContent
         return b
     }
 
-    function createImageWithSrc(src) {
-        const e = document.createElement('img')
-        e.src = src
-        return e
+    function createElementImg(tag, src) {
+        const i = document.createElement(tag)
+        i.src = src
+        return i
     }
 
     function createSongInformation(div) {
-        div.appendChild(createImageWithSrc('./static/img/img.png'))
+        div.appendChild(createElementImg('img', './static/img/img.png'))
         inputElements.forEach(x => {
             div.appendChild(createElementWithTextContent(x.name === 'date' ? 'h3' : 'h2', `${x.name.charAt(0).toUpperCase() + x.name.slice(1)}: ${x.value}`))
         })
+        return div
+
+
+    }
+
+    function createBtns(div) {
+        Array.from(['Save song!save-btn', 'Like song!like-btn', 'Delete!delete-btn']).forEach(x => {
+            div.appendChild(createButtonWithClassAndTextContent('button', ...x.split('!')))
+        })
+        return div
+    }
+
+    function removeBtns(div) {
+        Array.from(['.save-btn', '.like-btn']).forEach(x => {
+                        div.removeChild(div.querySelector(x))
+                    })
         return div
     }
 
@@ -59,33 +74,25 @@ function solve() {
             return
         }
 
-        const div = createSongInformation(createElementWithClass('div', 'hits-info'))
+        const div = createBtns(createSongInformation(createElementWithClass('div', 'hits-info')))
 
-        div.appendChild(createButtonWithClassAndTextContent('button', 'Save song', 'save-btn'))
-        div.appendChild(createButtonWithClassAndTextContent('button', 'Like song', 'like-btn'))
-        div.appendChild(createButtonWithClassAndTextContent('button', 'Delete', 'delete-btn'))
         allHits.appendChild(div)
         clearInputFields()
-
 
         const allButtons = Array.from(document.querySelectorAll('button'))
 
         allButtons.forEach(x => {
             x.addEventListener('click', (event) => {
+
                 if (x.textContent === 'Save song') {
-                    const div = x.parentElement
+                    const div = removeBtns(x.parentElement)
                     const savedContainer = document.querySelector('.saved-container')
 
-                    const saveBtnToRemove = div.querySelector('.save-btn')
-                    const likeBtnToRemove = div.querySelector('.like-btn')
-
-                    div.removeChild(saveBtnToRemove)
-                    div.removeChild(likeBtnToRemove)
-
-                    savedContainer.appendChild(div)
                     allHits.removeChild(div)
+                    savedContainer.appendChild(div)
 
                 } else if (x.textContent === 'Like song' && !x.disabled) {
+                    const likes = document.querySelector('.likes > p')
                     const [text, counter] = likes.textContent.split(': ')
                     likes.textContent = `${text}: ${Number(counter) + 1}`
                     x.disabled = true
