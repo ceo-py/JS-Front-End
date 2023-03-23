9ac0cb3eea9451c0cc04d4e33803ba5b4170f198
ceo-py <sfmf@abv.bg>
2023-03-23 21:55:55 +0200

    init

--- JS-Front-End-Exams/Exam Preparation I/03. Todo List/3_todo_list.js
+++ JS-Front-End-Exams/Exam Preparation I/03. Todo List/3_todo_list.js
@@ -1,106 +1,9 @@
 async function attachEvents() {
-    const [addBtn, loadAll] = document.querySelectorAll('button[type="submit"]')
-    const toDoList = document.querySelector('#todo-list')
+    const [addButton, loadButton] = document.querySelectorAll('button[type="submit"]')
     const title = document.querySelector('#title')
-    const tasksUrlApi = 'http://localhost:3030/jsonstore/tasks/'
+    const todoList = document.querySelector('#todo-list')
+    const API_URL = 'http://localhost:3030/jsonstore/tasks/'
 
-
-    const deleteFromApi = async (e) => {
-        const el = e.target.parentElement
-
-        await fetch(`${tasksUrlApi}${event.target.parentElement.id}`, {
-            method: 'DELETE'
-        })
-        deleteElementFromApi(el)
-    }
-
-    const deleteElementFromApi = (e) => {
-        e.remove()
-    }
-
-    const getDataFromDB = async () => {
-        const data = await fetch(tasksUrlApi)
-        return await data.json()
-    }
-
-    const createTaskForDB = (nameContent) => {
-        return {
-            name: nameContent
-        }
-    }
-
-    const addBtnFunctionality = async () => {
-        event.preventDefault()
-        await fetch(tasksUrlApi, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(createTaskForDB(title.value))
-        })
-
-        toDoList.innerHTML = ''
-
-        await loadDataToHtml(await getDataFromDB())
-    }
-
-    const loadBtnFunctionality = async () => {
-        event.preventDefault()
-        await loadDataToHtml(await getDataFromDB())
-
-    }
-
-    const removeFunctionality = async () => {
-        await deleteFromApi(event)
-    }
-
-    const editFunctionality = async () => {
-        const e = event.target
-        const el = e.parentElement
-
-        if (e.textContent === 'Edit') {
-
-            const foundSpan = el.querySelector('span')
-            el.replaceChild(createElement('input', '', e.parentElement.children[0].textContent), foundSpan)
-            e.textContent = 'Submit'
-        } else {
-            const foundInput = el.querySelector('input')
-            const newTitle = e.parentElement.children[0].value
-            el.replaceChild(createElement('span', '', e.parentElement.children[0].value), foundInput)
-            
-            
-            await fetch(`${tasksUrlApi}${e.parentElement.id}`, {
-                method: 'PATCH', header: {
-                    'content-type': 'application/json'
-                }, body: JSON.stringify({
-                    name: newTitle
-                })
-            })
-            e.textContent = 'Edit'
-            await loadDataToHtml(await getDataFromDB())
-        }
-    }
-
-    const createLiElement = async (data) => {
-        const li = createElement('li', '', '', '', {id: data._id})
-        li.appendChild(createElement('span', data.name))
-        const remove = createElement('button', 'Remove')
-        const edit = createElement('button', 'Edit')
-
-        remove.addEventListener('click', await removeFunctionality)
-        edit.addEventListener('click', await editFunctionality)
-        li.appendChild(remove)
-        li.appendChild(edit)
-        return li
-    }
-
-    const loadDataToHtml = async (data) => {
-        toDoList.innerHTML = ''
-        for (const key in data) {
-            toDoList.appendChild(await createLiElement(data[key]))
-        }
-    }
-
-    addBtn.addEventListener('click', await addBtnFunctionality)
-    loadAll.addEventListener('click', await loadBtnFunctionality)
 
     const createElement = (tag, textContent, value, className, attributes) => {
         const e = document.createElement(tag)
@@ -125,6 +28,255 @@ async function attachEvents() {
         return e
     }
 
+    const resetInput = (data) => {
+        data.forEach(x => x.value= '')
+    }
+
+    const loadDataFromApi = async () => {
+        const data = await fetch(API_URL)
+        return await data.json()
+    }
+
+    const deleteElementFromHTML = (e) => {
+        e.remove()
+    }
+
+    const removeBtnFunctionality = async () => {
+        const e = event.target.parentElement
+
+        await fetch(`${API_URL}${e.id}`, {
+            method: 'DELETE'
+        })
+
+        deleteElementFromHTML(e)
+
+
+    }
+
+    const editItemFromAPiDB = async (e, value) => {
+
+        await fetch(`${API_URL}${e.id}` , {
+            method: 'PATCH',
+            header: {'Content-Type': 'application/json'},
+            body: JSON.stringify(createItemForAPiDb(value))
+        })
+
+    }
+
+    const editBtnFunctionality = async () => {
+        const e = event.target
+        const eParent = e.parentElement
+
+        if (e.textContent === 'Edit') {
+
+            const span = eParent.querySelector('span')
+            eParent.replaceChild(createElement('input', '', span.textContent), span)
+
+
+            e.textContent = 'Submit'
+        } else {
+
+            const input = eParent.querySelector('input')
+            const intputValue = input.value
+            eParent.replaceChild(createElement('span', intputValue), input)
+
+            e.textContent = 'Edit'
+            await editItemFromAPiDB(eParent, intputValue)
+            await loadDataToHTML(await loadDataFromApi())
+        }
+
+    }
+
+    const createTaskElement = async (data) => {
+        const li = createElement('li', '', '', '', {id:data._id})
+        li.appendChild(createElement('span', data.name))
+
+        const remove = createElement('button', 'Remove')
+        const edit = createElement('button', 'Edit')
+
+        remove.addEventListener('click', await removeBtnFunctionality)
+        edit.addEventListener('click', await editBtnFunctionality)
+
+        li.appendChild(remove)
+        li.appendChild(edit)
+
+        return li
+    }
+
+    const loadDataToHTML = async (data) => {
+        todoList.innerHTML = ''
+
+        for (const key in data) {
+            todoList.appendChild(await createTaskElement(data[key]))
+        }
+
+    }
+
+    const createItemForAPiDb = (item) => {
+        return {
+            name: item
+        }
+    }
+
+    const addButtonFunctionality = async () => {
+        event.preventDefault()
+        await fetch(API_URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(createItemForAPiDb(title.value))
+        })
+        await loadDataToHTML(await loadDataFromApi())
+        resetInput([title])
+
+    }
+
+    const loadButtonFunctionality = async () => {
+        event.preventDefault()
+        await loadDataToHTML(await loadDataFromApi())
+    }
+
+    addButton.addEventListener('click', await addButtonFunctionality)
+    loadButton.addEventListener('click', await loadButtonFunctionality)
+
 }
 
 attachEvents();
+
+
+
+
+
+
+
+
+
+// async function attachEvents() {
+//     const [addBtn, loadAll] = document.querySelectorAll('button[type="submit"]')
+//     const toDoList = document.querySelector('#todo-list')
+//     const title = document.querySelector('#title')
+//     const tasksUrlApi = 'http://localhost:3030/jsonstore/tasks/'
+//
+//
+//     const deleteFromApi = async (e) => {
+//         const el = e.target.parentElement
+//
+//         await fetch(`${tasksUrlApi}${event.target.parentElement.id}`, {
+//             method: 'DELETE'
+//         })
+//         deleteElementFromApi(el)
+//     }
+//
+//     const deleteElementFromApi = (e) => {
+//         e.remove()
+//     }
+//
+//     const getDataFromDB = async () => {
+//         const data = await fetch(tasksUrlApi)
+//         return await data.json()
+//     }
+//
+//     const createTaskForDB = (nameContent) => {
+//         return {
+//             name: nameContent
+//         }
+//     }
+//
+//     const addBtnFunctionality = async () => {
+//         event.preventDefault()
+//         await fetch(tasksUrlApi, {
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify(createTaskForDB(title.value))
+//         })
+//
+//         toDoList.innerHTML = ''
+//
+//         await loadDataToHtml(await getDataFromDB())
+//     }
+//
+//     const loadBtnFunctionality = async () => {
+//         event.preventDefault()
+//         await loadDataToHtml(await getDataFromDB())
+//
+//     }
+//
+//     const removeFunctionality = async () => {
+//         await deleteFromApi(event)
+//     }
+//
+//     const editFunctionality = async () => {
+//         const e = event.target
+//         const el = e.parentElement
+//
+//         if (e.textContent === 'Edit') {
+//
+//             const foundSpan = el.querySelector('span')
+//             el.replaceChild(createElement('input', '', e.parentElement.children[0].textContent), foundSpan)
+//             e.textContent = 'Submit'
+//         } else {
+//             const foundInput = el.querySelector('input')
+//             const newTitle = e.parentElement.children[0].value
+//             el.replaceChild(createElement('span', '', e.parentElement.children[0].value), foundInput)
+//
+//
+//             await fetch(`${tasksUrlApi}${e.parentElement.id}`, {
+//                 method: 'PATCH', header: {
+//                     'content-type': 'application/json'
+//                 }, body: JSON.stringify({
+//                     name: newTitle
+//                 })
+//             })
+//             e.textContent = 'Edit'
+//             await loadDataToHtml(await getDataFromDB())
+//         }
+//     }
+//
+//     const createLiElement = async (data) => {
+//         const li = createElement('li', '', '', '', {id: data._id})
+//         li.appendChild(createElement('span', data.name))
+//         const remove = createElement('button', 'Remove')
+//         const edit = createElement('button', 'Edit')
+//
+//         remove.addEventListener('click', await removeFunctionality)
+//         edit.addEventListener('click', await editFunctionality)
+//         li.appendChild(remove)
+//         li.appendChild(edit)
+//         return li
+//     }
+//
+//     const loadDataToHtml = async (data) => {
+//         toDoList.innerHTML = ''
+//         for (const key in data) {
+//             toDoList.appendChild(await createLiElement(data[key]))
+//         }
+//     }
+//
+//     addBtn.addEventListener('click', await addBtnFunctionality)
+//     loadAll.addEventListener('click', await loadBtnFunctionality)
+//
+//     const createElement = (tag, textContent, value, className, attributes) => {
+//         const e = document.createElement(tag)
+//
+//         if (textContent) {
+//             e.textContent = textContent
+//         }
+//
+//         if (value) {
+//             e.value = value
+//         }
+//
+//         if (className) {
+//             className.forEach(x => e.classList.add(x))
+//         }
+//
+//         if (attributes) {
+//             for (const key in attributes) {
+//                 e.setAttribute(key, attributes[key])
+//             }
+//         }
+//         return e
+//     }
+//
+// }
+//
+// attachEvents();
---
+++ JS-Front-End-Exams/Exam Preparation II/03. Grocery List/3_grocery_list.js
@@ -0,0 +1,144 @@
+function attachEvents() {
+    const API_URL = 'http://localhost:3030/jsonstore/grocery/'
+    const addProduct = document.querySelector('#add-product')
+    const tbody = document.querySelector('#tbody')
+    const updateProduct = document.querySelector('#update-product')
+    const loadProduct = document.querySelector('#load-product')
+    const product = document.querySelector('#product')
+    const count = document.querySelector('#count')
+    const price = document.querySelector('#price')
+    let updateBtnId = null
+
+
+    const createElement = (tag, textContent, value, className, attributes) => {
+        const e = document.createElement(tag)
+
+        if (textContent) {
+            e.textContent = textContent
+        }
+
+        if (value) {
+            e.value = value
+        }
+
+        if (className) {
+            className.forEach(x => e.classList.add(x))
+        }
+
+        if (attributes) {
+            for (const key in attributes) {
+                e.setAttribute(key, attributes[key])
+            }
+        }
+        return e
+    }
+
+    const resetInput = (data) => {
+        data.forEach(x => x.value = '')
+    }
+
+    const loadDataFromApi = async () => {
+        const data = await fetch(API_URL)
+        return await data.json()
+    }
+
+    const updateBtnFunctionality = async () => {
+        const e = event.target.parentElement.parentElement
+
+        product.value = e.querySelector('.name').textContent
+        count.value = e.querySelector('.count-product').textContent
+        price.value = e.querySelector('.product-price').textContent
+
+        updateBtnId = event.target.id
+        updateProduct.disabled = false
+        addProduct.disabled = true
+    }
+
+    const deleteBtnFunctionality = async () => {
+        const e = event.target
+
+        await fetch(`${API_URL}${e.id}`, {
+            method: 'DELETE'
+        })
+
+        await loadDataToHTML(await loadDataFromApi())
+    }
+
+    const createTaskElement = async (data) => {
+        const tr = createElement('tr')
+
+        tr.appendChild(createElement('td', data.product, '', ['name']))
+        tr.appendChild(createElement('td', data.count, '', ['count-product']))
+        tr.appendChild(createElement('td', data.price, '', ['product-price']))
+
+        const td = createElement('td', '', '', ['btn'])
+
+        const updateBtn = createElement('button', 'Update', '', ['update'], {id: data._id})
+        updateBtn.addEventListener('click', await updateBtnFunctionality)
+
+        const deleteBtn = createElement('button', 'Delete', '', ['delete'], {id: data._id})
+        deleteBtn.addEventListener('click', await deleteBtnFunctionality)
+
+        td.appendChild(updateBtn)
+        td.appendChild(deleteBtn)
+
+        tr.appendChild(td)
+
+        return tr
+    }
+
+    const loadDataToHTML = async (data) => {
+        tbody.innerHTML = ''
+
+        for (const key in data) {
+            tbody.appendChild(await createTaskElement(data[key]))
+        }
+
+    }
+
+    const loadProductFunctionality = async () => {
+        event.preventDefault()
+        await loadDataToHTML(await loadDataFromApi())
+    }
+
+    const createItemForAPiDb = (product, count, price) => {
+        return {
+            product, count, price
+        }
+    }
+
+    const addProductFunctionality = async () => {
+        event.preventDefault()
+
+        await fetch(API_URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(createItemForAPiDb(product.value, count.value, price.value))
+        })
+        await loadDataToHTML(await loadDataFromApi())
+        resetInput([product, count, price])
+    }
+
+    const updateProductFunctionality = async () => {
+
+        await fetch(`${API_URL}${updateBtnId}`, {
+            method: 'PATCH',
+            header: {'Content-Type': 'application/json'},
+            body: JSON.stringify(createItemForAPiDb(product.value, count.value, price.value))
+        })
+
+        await loadDataToHTML(await loadDataFromApi())
+        resetInput([product, count, price])
+        updateProduct.disabled = true
+        addProduct.disabled = false
+    }
+
+    loadProduct.addEventListener('click', loadProductFunctionality)
+    addProduct.addEventListener('click', addProductFunctionality)
+    updateProduct.addEventListener('click', updateProductFunctionality)
+
+
+}
+
+
+attachEvents();
