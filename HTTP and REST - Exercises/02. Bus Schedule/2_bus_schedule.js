39fd1bfad5bbbd189a59c11e0ffdbff007010f9a
ceo-py <sfmf@abv.bg>
2023-03-27 10:40:39 +0300

    initial

--- HTTP and REST - Exercises/02. Bus Schedule/2_bus_schedule.js
+++ HTTP and REST - Exercises/02. Bus Schedule/2_bus_schedule.js
@@ -4,7 +4,7 @@ function solve() {
     const apiURL = 'http://localhost:3030/jsonstore/bus/schedule/'
     let [departId, departLocation] = ['depot', null]
 
-
+    departBtn.innerHTML =
     function depart() {
         fetch(`${apiURL}${departId}`).then(x => x.json()).then(x => {
             departLocation = x['name']
@@ -30,3 +30,43 @@ function solve() {
 }
 
 let result = solve();
+
+
+// function solve() {
+//     const info = document.querySelector('.info')
+//     const [departBtn, arriveBtn] = document.querySelectorAll('input')
+//     const apiURL = 'http://localhost:3030/jsonstore/bus/schedule/'
+//     let [departId, departLocation] = ['depot', null]
+//
+//
+//
+//     async function depart() {
+//         let data = await fetch(`${apiURL}${departId}`)
+//         data = await data.json()
+//
+//         departLocation = data['name']
+//         info.textContent = `Next stop ${departLocation}`
+//         departId = data['next']
+//         departBtn.disabled = true
+//         arriveBtn.disabled = false
+//     }
+//
+//     async function arrive() {
+//         info.textContent = `Arriving at ${departLocation}`
+//         departBtn.disabled = false
+//         arriveBtn.disabled = true
+//
+//     }
+//
+//
+//     return {
+//         depart,
+//         arrive
+//     };
+// }
+//
+// let result = solve();
+
+
+
+
