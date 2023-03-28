4a98f1170804d8868730c7cce8b46d593b65d41f
ceo-py <sfmf@abv.bg>
2023-03-28 12:46:59 +0300

    ref

--- JS-Front-End-Exams/Exam Preparation I/01. The Pianist/1_the_pianist.js
+++ JS-Front-End-Exams/Exam Preparation I/01. The Pianist/1_the_pianist.js
@@ -3,25 +3,20 @@ function pianistCatalog(list) {
     list.pop()
     let catalog = {}
 
-    const Add = (piece, composer, key) => {
+    const commands = {
+        Add: (piece, composer, key) => {
         return catalog.hasOwnProperty(piece) ? `${piece} is already in the collection!` :
             (catalog[piece] = new Piece(piece, composer, key),
-                `${piece} by ${composer} in ${key} added to the collection!`)
-    }
+                `${piece} by ${composer} in ${key} added to the collection!`)},
 
-    const Remove = (piece) => {
+        Remove: (piece) => {
         return catalog[piece] ? (delete catalog[piece], `Successfully removed ${piece}!`)
-            : `Invalid operation! ${piece} does not exist in the collection.`
-    }
+            : `Invalid operation! ${piece} does not exist in the collection.`},
 
-    const ChangeKey = (piece, key) => {
+        ChangeKey: (piece, key) => {
         return catalog.hasOwnProperty(piece) ? (catalog[piece].changeKey(key),
                 `Changed the key of ${piece} to ${key}!`)
-            : `Invalid operation! ${piece} does not exist in the collection.`
-    }
-
-    const commands = {
-        Add, Remove, ChangeKey
+            : `Invalid operation! ${piece} does not exist in the collection.`}
     }
 
 
