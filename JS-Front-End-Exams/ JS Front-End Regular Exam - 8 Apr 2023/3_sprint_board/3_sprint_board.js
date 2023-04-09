addc151c0fb2a0f3ebd4a0eec82bfff9330edca0
ceo-py <sfmf@abv.bg>
2023-04-09 17:32:26 +0300

    init

---
+++ JS-Front-End-Exams/ JS Front-End Regular Exam - 8 Apr 2023/3_sprint_board/3_sprint_board.js
@@ -0,0 +1,168 @@
+function attachEvents() {
+    const API_URL = 'http://localhost:3030/jsonstore/tasks/'
+    const inputFields = {
+        title: document.querySelector('#title'),
+        description: document.querySelector('#description'),
+    }
+
+    const sprintApp = {
+        toDo: document.querySelector('#todo-section ul'),
+        inProgress: document.querySelector('#in-progress-section ul'),
+        review: document.querySelector('#code-review-section ul'),
+        done: document.querySelector('#done-section ul'),
+        createBtn: document.querySelector('#create-task-btn'),
+        loadBtn: document.querySelector('#load-board-btn'),
+
+    }
+
+    const board = {
+        'ToDo': {
+            html: sprintApp.toDo,
+            btnText: 'Move to In Progress'
+        },
+        'In Progress': {
+            html: sprintApp.inProgress,
+            btnText: 'Move to Code Review'
+        },
+        'Code Review': {
+            html: sprintApp.review,
+            btnText: 'Move to Done'
+        },
+        'Done': {
+            html: sprintApp.done,
+            btnText: 'Close'
+        },
+    }
+
+    const moveBtnStatus = {
+        'Move to In Progress': 'In Progress',
+        'Move to Code Review': 'Code Review',
+        'Move to Done': 'Done',
+    }
+
+    const moveBtnFunctionality = async (even) => {
+
+        const btnTextContent = even.currentTarget
+
+        await apiRequest({
+            url: `${API_URL}${btnTextContent.id}`,
+            method: btnTextContent.textContent === 'Close' ? 'DELETE' : 'PATCH',
+            item: {
+                status: moveBtnStatus[btnTextContent.textContent]
+            }
+        })
+        loadApiDataToHtml(await apiRequest({
+            url: API_URL,
+            method: 'GET'
+        }))
+    }
+
+    const apiRequest = async ({url = '', method = '', id = '', item = ''}) => {
+        const options = {
+            method: method
+        }
+        if (['PATCH', 'POST'].includes(method)) {
+            options.headers = {'Content-Type': 'application/json'}
+            options.body = JSON.stringify(item)
+        }
+        const data = await fetch(`${url}${id}`, options)
+        return await data.json()
+    }
+
+
+    const createElement = ({
+                               tag,
+                               textContent = '',
+                               value = '',
+                               className = [],
+                               attributes = {},
+                               buttonEvent = {},
+                               parent = ''
+                           }) => {
+        const e = document.createElement(tag)
+        if (textContent) e.textContent = textContent
+        if (value) e.value = value
+        className.forEach(x => e.classList.add(x))
+        for (const [key, value] of Object.entries(attributes)) {
+            e.setAttribute(key, value)
+        }
+        for (const [key, value] of Object.entries(buttonEvent)) {
+            e.addEventListener(key, value)
+        }
+        if (parent) {
+            parent.appendChild(e)
+        } else {
+            return e
+        }
+
+    }
+
+    const clearInputFields = (dataFromInput => dataFromInput.forEach(x => x.value = ''))
+
+
+    const createBtnFunctionality = async (event) => {
+        event.preventDefault()
+
+        await apiRequest({
+            url: API_URL,
+            method: 'POST',
+            item: {
+                title: inputFields.title.value,
+                description: inputFields.description.value,
+                status: 'ToDo'
+            }
+        })
+        clearInputFields(Object.values(inputFields))
+        loadApiDataToHtml(await apiRequest({
+            url: API_URL,
+            method: 'GET'
+        }))
+    }
+
+    const createHtmlTaskElement = (data) => {
+        const li = createElement({
+            tag: 'li',
+            className: ['task'],
+        })
+        createElement({
+            tag: 'h3',
+            textContent: data.title,
+            parent: li
+        })
+        createElement({
+            tag: 'p',
+            textContent: data.description,
+            parent: li
+        })
+        createElement({
+            tag: 'button',
+            textContent: board[data.status].btnText,
+            attributes: {id: data._id},
+            buttonEvent: {click: moveBtnFunctionality},
+            parent: li
+        })
+
+        return li
+
+    }
+
+    const loadApiDataToHtml = (data) => {
+        Object.values(board).forEach(x => x.html.innerHTML = '')
+        for (const item of Object.values(data))
+            board[item.status].html.appendChild(createHtmlTaskElement(item))
+    }
+
+    const loadBtnFunctionality = async (event) => {
+        event.preventDefault()
+        loadApiDataToHtml(await apiRequest({
+            url: API_URL,
+            method: 'GET'
+        }))
+    }
+
+    sprintApp.createBtn.addEventListener('click', createBtnFunctionality)
+    sprintApp.loadBtn.addEventListener('click', loadBtnFunctionality)
+
+}
+
+attachEvents();
