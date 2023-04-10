310f1c7c00027dbbfa1ca8a3bfe7a1dc22a34fdc
ceo-py <sfmf@abv.bg>
2023-04-10 13:20:01 +0300

    init

--- JS-Front-End-Exams/ JS Front-End Regular Exam - 8 Apr 2023/1_sprint_review/1_sprint_review.js
+++ JS-Front-End-Exams/ JS Front-End Regular Exam - 8 Apr 2023/1_sprint_review/1_sprint_review.js
@@ -1,6 +1,12 @@
 function springReview(list) {
     const inputs = list.shift()
     const taskBoard = {}
+    const keyDontExist = (key) => {
+        if (!taskBoard.hasOwnProperty(key)) {
+            console.log(`Assignee ${key} does not exist on the board!`)
+            return true
+        }
+    }
 
     for (const i in list) {
         const info = list[i].split(':')
@@ -21,10 +27,7 @@ function springReview(list) {
                 })
 
             } else if (info[0] === 'Change Status') {
-                if (!taskBoard.hasOwnProperty(info[1])) {
-                    console.log(`Assignee ${info[1]} does not exist on the board!`)
-                    continue
-                }
+                if (keyDontExist(info[1])) continue
                 const task = taskBoard[info[1]].filter(x => x.taskId === info[2])
                 if (task.length === 0) {
                     console.log(`Task with ID ${info[2]} does not exist for ${info[1]}!`)
@@ -32,10 +35,7 @@ function springReview(list) {
                 }
                 task[0].status = info[3]
             } else if (info[0] === 'Remove Task') {
-                if (!taskBoard.hasOwnProperty(info[1])) {
-                    console.log(`Assignee ${info[1]} does not exist on the board!`)
-                    continue
-                }
+                if (keyDontExist(info[1])) continue
                 const index = Number(info[2])
                 if (index < 0 || taskBoard[info[1]].length < index) {
                     console.log('Index is out of range!')
@@ -53,34 +53,29 @@ function springReview(list) {
         'Done': 0,
     }
 
-    for (const items of Object.values(taskBoard)) {
-        items.forEach(x => points[x.status] += x.estimatedPoints)
-    }
+    Object.values(taskBoard).forEach(items => items.forEach(x => points[x.status] += x.estimatedPoints));
 
     console.log(`ToDo: ${points.ToDo}pts`)
     console.log(`In Progress: ${points["In Progress"]}pts`)
     console.log(`Code Review: ${points["Code Review"]}pts`)
     console.log(`Done Points: ${points.Done}pts`)
 
-    if (points.Done >= points.ToDo + points["In Progress"] +points["Code Review"]) {
-        console.log('Sprint was successful!')
-    } else {
-        console.log('Sprint was unsuccessful...')
-    }
+    points.Done >= points.ToDo + points["In Progress"] + points["Code Review"] ? console.log('Sprint was successful!') : console.log('Sprint was unsuccessful...');
 
 }
 
 // springReview(['5', 'Kiril:BOP-1209:Fix Minor Bug:ToDo:3', 'Mariya:BOP-1210:Fix Major Bug:In Progress:3', 'Peter:BOP-1211:POC:Code Review:5', 'Georgi:BOP-1212:Investigation Task:Done:2', 'Mariya:BOP-1213:New Account Page:In Progress:13', 'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5', 'Change Status:Peter:BOP-1290:ToDo', 'Remove Task:Mariya:1', 'Remove Task:Joro:1',])
 
-springReview( [
-        '4',
-        'Kiril:BOP-1213:Fix Typo:Done:1',
-        'Peter:BOP-1214:New Products Page:In Progress:2',
-        'Mariya:BOP-1215:Setup Routing:ToDo:8',
-        'Georgi:BOP-1216:Add Business Card:Code Review:3',
-        'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
-        'Change Status:Georgi:BOP-1216:Done',
-        'Change Status:Will:BOP-1212:In Progress',
-        'Remove Task:Georgi:3',
-        'Change Status:Mariya:BOP-1215:Done',
-    ])
+
+springReview([
+    '4',
+    'Kiril:BOP-1213:Fix Typo:Done:1',
+    'Peter:BOP-1214:New Products Page:In Progress:2',
+    'Mariya:BOP-1215:Setup Routing:ToDo:8',
+    'Georgi:BOP-1216:Add Business Card:Code Review:3',
+    'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
+    'Change Status:Georgi:BOP-1216:Done',
+    'Change Status:Will:BOP-1212:In Progress',
+    'Remove Task:Georgi:3',
+    'Change Status:Mariya:BOP-1215:Done',
+])
