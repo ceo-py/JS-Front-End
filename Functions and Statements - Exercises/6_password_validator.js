1021f52d7e84efdd8bf4125807a02c991f735791
ceo-py <sfmf@abv.bg>
2023-07-07 13:20:47 +0300

    added another solution

--- Functions and Statements - Exercises/6_password_validator.js
+++ Functions and Statements - Exercises/6_password_validator.js
@@ -1,23 +1,40 @@
 function solve(password) {
     let output = [];
 
-    if (password.length <= 5 || password.length > 10) {
-        output.push("Password must be between 6 and 10 characters")
-    }
-    if (!/^[a-zA-Z0-9]+$/.test(password)) {
-        output.push("Password must consist only of letters and digits")
-    }
-    if (!/\d.*\d/.test(password)) {
-        output.push("Password must have at least 2 digits")
-    }
-    if (output.length > 0) {
-        console.log(`${output.join('\n')}`)
-    } else {
-        console.log('Password is valid')
-    }
+    if (password.length <= 5 || password.length > 10) output.push("Password must be between 6 and 10 characters")
+
+    if (!/^[a-zA-Z0-9]+$/.test(password)) output.push("Password must consist only of letters and digits")
+
+    if (!/\d.*\d/.test(password)) output.push("Password must have at least 2 digits")
+
+    console.log(output.length > 0 ? `${output.join('\n')}` : 'Password is valid')
 }
 
 solve('Pa$s$s')
+
+
+
+
+// function solve(password) {
+//     let output = [];
+//
+//     if (password.length <= 5 || password.length > 10) {
+//         output.push("Password must be between 6 and 10 characters")
+//     }
+//     if (!/^[a-zA-Z0-9]+$/.test(password)) {
+//         output.push("Password must consist only of letters and digits")
+//     }
+//     if (!/\d.*\d/.test(password)) {
+//         output.push("Password must have at least 2 digits")
+//     }
+//     if (output.length > 0) {
+//         console.log(`${output.join('\n')}`)
+//     } else {
+//         console.log('Password is valid')
+//     }
+// }
+//
+// solve('Pa$s$s')
 
 
 
