2d8f14ccab758f38dfd598d435936a0ad0ecc2c0
ceo-py <sfmf@abv.bg>
2023-08-03 12:30:43 +0300

    added another solution

--- DOM and Events - Lab/06. Add - Delete/06_add_delete.js
+++ DOM and Events - Lab/06. Add - Delete/06_add_delete.js
@@ -16,3 +16,26 @@ function addItem() {
     }
 }
 
+
+
+// function addItem() {
+//     const newItemText = document.getElementById("newItemText").value.trim();
+//     const listItems = document.getElementById("items");
+//     listItems.addEventListener('click', deleteItem);
+//
+//     if (newItemText !== "") {
+//         const newItem = document.createElement("li");
+//         const textNode = document.createTextNode(newItemText);
+//         const deleteLink = document.createElement("a");
+//         deleteLink.setAttribute("href", "#");
+//         deleteLink.textContent = "[Delete]";
+//         newItem.appendChild(textNode);
+//         newItem.appendChild(deleteLink);
+//         listItems.appendChild(newItem);
+//         document.getElementById("newItemText").value = "";
+//     }
+//
+//     function deleteItem(item) {
+//         item.target.parentNode.remove();
+//     }
+// }
