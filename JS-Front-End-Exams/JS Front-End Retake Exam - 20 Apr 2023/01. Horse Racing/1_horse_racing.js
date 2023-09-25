9b068daef7bb05204b21638cc5944680be03273a
ceo-py <sfmf@abv.bg>
2023-09-25 22:10:27 +0300

    added another solution with indexOf .at index judge is not supporting!!!

--- JS-Front-End-Exams/JS Front-End Retake Exam - 20 Apr 2023/01. Horse Racing/1_horse_racing.js
+++ JS-Front-End-Exams/JS Front-End Retake Exam - 20 Apr 2023/01. Horse Racing/1_horse_racing.js
@@ -63,3 +63,48 @@ horseRace(['Onyx|Domino|Sugar|Fiona',
 // 'Trouble Lilly',
 // 'Finish',
 // 'Rage Lilly'])
+
+
+// function horseRace(list) {
+//     const horses = list.shift().split('|')
+//
+//     const findHorsePos = (horse) => {
+//         return horses.indexOf(horse)
+//     }
+//
+//     const horseSwap = (firstHorseIndex, secondHorseIndex) => {
+//         [horses[firstHorseIndex], horses[secondHorseIndex]] = [horses[secondHorseIndex], horses[firstHorseIndex]]
+//     }
+//
+//     for (const index in list) {
+//         const [command, overtakingOrName, overtakenOrName] = list[index].split(' ')
+//         if (command === 'Finish') break
+//         if (command === 'Retake') {
+//             const firstHorse = findHorsePos(overtakingOrName)
+//             const secondHorse = findHorsePos(overtakenOrName)
+//             if (firstHorse >= 0 && secondHorse > 0 && firstHorse < secondHorse) {
+//                 horseSwap(firstHorse, secondHorse)
+//                 console.log(`${overtakingOrName} retakes ${overtakenOrName}.`)
+//             }
+//         } else if (command === 'Trouble') {
+//             const horse = findHorsePos(overtakingOrName)
+//             if (horse > 0) {
+//                 horseSwap(horse - 1, horse)
+//                 console.log(`Trouble for ${overtakingOrName} - drops one position.`)
+//             }
+//         } else if (command === 'Rage') {
+//             const horse = findHorsePos(overtakingOrName)
+//             horses.splice(horse, 1)
+//             horses.splice(horse + 2, 0, overtakingOrName)
+//             console.log(`${overtakingOrName} rages 2 positions ahead.`)
+//
+//
+//         } else if (command === 'Miracle') {
+//             const currentHorse = horses.shift()
+//             horses.push(currentHorse)
+//             console.log(`What a miracle - ${currentHorse} becomes first.`)
+//         }
+//     }
+//     console.log(horses.join('->'))
+//     console.log(`The winner is: ${horses.pop()}`)
+// }
