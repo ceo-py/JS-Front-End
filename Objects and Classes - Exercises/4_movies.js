78e7e25b52020637ea124d0a15957186f1cd14c4
ceo-py <sfmf@abv.bg>
2023-10-27 19:58:18 +0300

    added new solution

--- Objects and Classes - Exercises/4_movies.js
+++ Objects and Classes - Exercises/4_movies.js
@@ -1,44 +1,60 @@
 function solve(list) {
-    let output = {}
-
-    for (item of list) {
+    const movies = {}
+    for (const item of list) {
         if (item.includes('addMovie')) {
-            let name = item.split('addMovie ')[1]
-            output[name] = {}
-            output[name].name = name
-        } else if (item.includes('directedBy')) {
-            let [nameMovie, nameDirector] = item.split(' directedBy ')
-            if (nameMovie in output) {
-                output[nameMovie].director = nameDirector
-            }
-        } else if (item.includes('onDate')) {
-            let [nameMovie, date] = item.split(' onDate ')
-            if (nameMovie in output) {
-                output[nameMovie].date = date
-            }
+            const name = item.split('addMovie ')[1]
+            movies[name] = {}
+            movies[name].name = name
+        }
+        try {
+            const [nameMovie, nameDirector] = item.split(' directedBy ')
+            movies[nameMovie].director = nameDirector
+        } catch (e) {
+        }
+        try {
+            const [nameMovie, date] = item.split(' onDate ')
+            movies[nameMovie].date = date
+        } catch (e) {
         }
     }
-    let outputRdy = []
-    for (let [key, value] of Object.entries(output)){
-        if (Object.keys(output[key]).length !== 3){
-            delete output[key]
-        } else {
-            outputRdy.push(value)
-            console.log(JSON.stringify(value))
-        }
-    }
+    Object.keys(movies).forEach(m => Object.entries(movies[m]).length !== 3? null: console.log(JSON.stringify(movies[m])))
 }
 
 
-solve([
-    'addMovie Fast and Furious',
-    'addMovie Godfather',
-    'Inception directedBy Christopher Nolan',
-    'Godfather directedBy Francis Ford Coppola',
-    'Godfather onDate 29.07.2018',
-    'Fast and Furious onDate 30.07.2018',
-    'Batman onDate 01.08.2018',
-    'Fast and Furious directedBy Rob Cohen'
-])
+
+
+// function solve(list) {
+//     let output = {}
+//
+//     for (item of list) {
+//         if (item.includes('addMovie')) {
+//             let name = item.split('addMovie ')[1]
+//             output[name] = {}
+//             output[name].name = name
+//         } else if (item.includes('directedBy')) {
+//             let [nameMovie, nameDirector] = item.split(' directedBy ')
+//             if (nameMovie in output) {
+//                 output[nameMovie].director = nameDirector
+//             }
+//         } else if (item.includes('onDate')) {
+//             let [nameMovie, date] = item.split(' onDate ')
+//             if (nameMovie in output) {
+//                 output[nameMovie].date = date
+//             }
+//         }
+//     }
+//     let outputRdy = []
+//     for (let [key, value] of Object.entries(output)){
+//         if (Object.keys(output[key]).length !== 3){
+//             delete output[key]
+//         } else {
+//             outputRdy.push(value)
+//             console.log(JSON.stringify(value))
+//         }
+//     }
+// }
+
+
+solve(['addMovie Fast and Furious', 'addMovie Godfather', 'Inception directedBy Christopher Nolan', 'Godfather directedBy Francis Ford Coppola', 'Godfather onDate 29.07.2018', 'Fast and Furious onDate 30.07.2018', 'Batman onDate 01.08.2018', 'Fast and Furious directedBy Rob Cohen'])
 
 
