73ff8af87f337aacb98c5c68eb08f52eb9a9d2a6
ceo-py <sfmf@abv.bg>
2023-10-27 16:55:21 +0300

    added new solution

--- Functions and Statements - Exercises/5_palindrome_integers.js
+++ Functions and Statements - Exercises/5_palindrome_integers.js
@@ -1,10 +1,14 @@
-
 function solve(list){
-    for (let number of list){
-        number = number.toString()
-        console.log(`${number === number.split("").reverse().join("")}`)
-    }
+    list.map(x => console.log(`${x.toString() === x.toString().split("").reverse().join("")}`))
 }
+
+
+// function solve(list){
+//     for (let number of list){
+//         number = number.toString()
+//         console.log(`${number === number.split("").reverse().join("")}`)
+//     }
+// }
 
 
 solve([123,323,421,121])
