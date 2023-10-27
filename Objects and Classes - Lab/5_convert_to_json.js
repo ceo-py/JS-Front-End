a6d3de7b9722109b044a66eab2117f3d1c9f1523
ceo-py <sfmf@abv.bg>
2023-10-27 18:17:25 +0300

    added new solution

--- Objects and Classes - Lab/5_convert_to_json.js
+++ Objects and Classes - Lab/5_convert_to_json.js
@@ -1,9 +1,19 @@
-function solve(...list) {
+function solve(name, lastName, hairColor) {
     const person = {
-        name: list.shift(), lastName: list.shift(), hairColor: list.shift()
+        name,
+        lastName,
+        hairColor
     }
     console.log(JSON.stringify(person))
 }
+
+
+// function solve(...list) {
+//     const person = {
+//         name: list.shift(), lastName: list.shift(), hairColor: list.shift()
+//     }
+//     console.log(JSON.stringify(person))
+// }
 
 
 solve('George', 'Jones', 'Brown')
