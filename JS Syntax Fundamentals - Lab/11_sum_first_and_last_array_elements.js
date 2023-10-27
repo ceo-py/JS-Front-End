b781dac2e64999a70e6bd4fd751f77653571d6e6
ceo-py <sfmf@abv.bg>
2023-10-27 15:57:14 +0300

    added new solution

--- JS Syntax Fundamentals - Lab/11_sum_first_and_last_array_elements.js
+++ JS Syntax Fundamentals - Lab/11_sum_first_and_last_array_elements.js
@@ -1,5 +1,10 @@
 function solve (numbers) {
-    console.log(`${numbers[0] + numbers.pop()}`)
+    console.log(`${numbers.shift() + numbers.pop()}`)
 }
+
+
+// function solve (numbers) {
+//     console.log(`${numbers[0] + numbers.pop()}`)
+// }
 
 solve([20, 30, 40])
