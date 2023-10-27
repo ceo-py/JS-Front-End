c250e92ede301c64c0ec2c618702b7a590a061aa
ceo-py <sfmf@abv.bg>
2023-10-27 15:04:26 +0300

    added new solution

--- JS Syntax Fundamentals - Lab/7_theatre_promotions.js
+++ JS Syntax Fundamentals - Lab/7_theatre_promotions.js
@@ -1,20 +1,13 @@
-
-function solve (day, age) {
-    function ticketPrice (age, ticketMinPrice, ticketMidPrice, ticketMaxPrice) {
-    if (age >= 0 && age <= 18){
-            return  `${ticketMinPrice}$`;
-        }
-        else if (age > 18 && age <= 64){
-            return  `${ticketMidPrice}$`
-        }
-        else if (age > 64 && age <= 122){
-            return  `${ticketMaxPrice}$`
-        } else {
-            return "Error!"
-        }
+function solve(day, age) {
+    function ticketPrice(age, ticketMinPrice, ticketMidPrice, ticketMaxPrice) {
+        if (age < 0 || age > 122) return "Error!"
+        else if (age <= 18) return `${ticketMinPrice}$`
+        else if (age <= 64) return `${ticketMidPrice}$`
+        else if (age <= 122) return `${ticketMaxPrice}$`
     }
+
     if (day === 'Weekday') {
-        console.log(ticketPrice(age,  12, 18, 12))
+        console.log(ticketPrice(age, 12, 18, 12))
     } else if (day === 'Weekend') {
         console.log(ticketPrice(age, 15, 20, 15))
     } else if (day === 'Holiday') {
@@ -22,4 +15,36 @@ function solve (day, age) {
     }
 }
 
-solve('Weekday', 42)
+
+
+
+
+
+// function solve (day, age) {
+//     function ticketPrice (age, ticketMinPrice, ticketMidPrice, ticketMaxPrice) {
+//     if (age >= 0 && age <= 18){
+//             return  `${ticketMinPrice}$`;
+//         }
+//         else if (age > 18 && age <= 64){
+//             return  `${ticketMidPrice}$`
+//         }
+//         else if (age > 64 && age <= 122){
+//             return  `${ticketMaxPrice}$`
+//         } else {
+//             return "Error!"
+//         }
+//     }
+//     if (day === 'Weekday') {
+//         console.log(ticketPrice(age,  12, 18, 12))
+//     } else if (day === 'Weekend') {
+//         console.log(ticketPrice(age, 15, 20, 15))
+//     } else if (day === 'Holiday') {
+//         console.log(ticketPrice(age, 5, 12, 10))
+//     }
+// }
+
+
+
+
+
+solve('Holiday', -12)
