ba8f04c191b92594b5e6cd02b094781e81f90b2b
ceo-py <sfmf@abv.bg>
2023-10-27 15:12:09 +0300

    added new solution

--- JS Syntax Fundamentals - Exercises/1_ages.js
+++ JS Syntax Fundamentals - Exercises/1_ages.js
@@ -1,17 +1,30 @@
 function solve(age) {
-    let output = 'out of bounds';
-    if (age >= 0 && age <= 2) {
-        output = 'baby';
-    } else if (age >= 3 && age <= 13) {
-        output = 'child';
-    } else if (age >= 14 && age <= 19) {
-        output = 'teenager';
-    } else if (age >= 20 && age <= 65) {
-        output = 'adult';
-    } else if (age >= 66) {
-        output = 'elder';
-    }
+    let output = null
+    if(age < 0)  output = 'out of bounds'
+    else if (age <= 2)  output = 'baby'
+    else if (age <= 13) output = 'child'
+    else if (age <= 19) output = 'teenager'
+    else if (age <= 65) output = 'adult'
+    else if (age >= 66) output = 'elder'
     console.log(output)
 }
+
+
+
+// function solve(age) {
+//     let output = 'out of bounds';
+//     if (age >= 0 && age <= 2) {
+//         output = 'baby';
+//     } else if (age >= 3 && age <= 13) {
+//         output = 'child';
+//     } else if (age >= 14 && age <= 19) {
+//         output = 'teenager';
+//     } else if (age >= 20 && age <= 65) {
+//         output = 'adult';
+//     } else if (age >= 66) {
+//         output = 'elder';
+//     }
+//     console.log(output)
+// }
 
 solve(100)
