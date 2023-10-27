9686c0afad8d85f2950f704143f390f8dfa4a5d5
ceo-py <sfmf@abv.bg>
2023-10-27 16:27:05 +0300

    added new solution

--- JS Syntax Fundamentals - Exercises/18_modern_times_of_hashtag.js
+++ JS Syntax Fundamentals - Exercises/18_modern_times_of_hashtag.js
@@ -1,16 +1,20 @@
-
-
 function solve(string){
-    string = string.split(' ')
-    function onlyLettersAndNumbers(str) {
-  return Boolean(str.match(/#[A-Za-z]/));
-    }
-    for (const word of string){
-        if (onlyLettersAndNumbers(word)){
-            console.log(word.slice(1, word.length))
-        }
-    }
+    string.split(' ').forEach(w => w.match(/#[A-Za-z]/)? console.log(w.slice(1, w.length)): null)
 }
+
+
+
+// function solve(string){
+//     string = string.split(' ')
+//     function onlyLettersAndNumbers(str) {
+//   return Boolean(str.match(/#[A-Za-z]/));
+//     }
+//     for (const word of string){
+//         if (onlyLettersAndNumbers(word)){
+//             console.log(word.slice(1, word.length))
+//         }
+//     }
+// }
 
 
 solve('Nowadays everyone uses # to tag a #special word in #socialMedia #')
