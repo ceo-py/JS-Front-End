5b2bc1d1a430f3d364979d3227712c2098912e97
ceo-py <sfmf@abv.bg>
2023-10-27 16:37:09 +0300

    added new solution

--- JS Syntax Fundamentals - Exercises/19_string_substring.js
+++ JS Syntax Fundamentals - Exercises/19_string_substring.js
@@ -1,17 +1,21 @@
-
-
 function solve(wordCheck, string){
-    string = string.split(' ')
-    let output = `${wordCheck} not found!`
-    for (let word of string){
-        word = word.toLowerCase()
-        if (word === wordCheck.toLowerCase()){
-            output = word
-            break
-        }
-    }
-    console.log(output)
+    console.log(string.split(' ').find(w => w.toLowerCase() === wordCheck.toLowerCase()) ? wordCheck : `${wordCheck} not found!`)
 }
+
+
+
+// function solve(wordCheck, string){
+//     string = string.split(' ')
+//     let output = `${wordCheck} not found!`
+//     for (let word of string){
+//         word = word.toLowerCase()
+//         if (word === wordCheck.toLowerCase()){
+//             output = word
+//             break
+//         }
+//     }
+//     console.log(output)
+// }
 
 
 solve('javascript',
