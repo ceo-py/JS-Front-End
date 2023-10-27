1ace1838811e013b7c5d46095f2b47a12464e85a
ceo-py <sfmf@abv.bg>
2023-10-27 14:21:41 +0300

    added new solution

--- JS Syntax Fundamentals - Lab/4_month_printer.js
+++ JS Syntax Fundamentals - Lab/4_month_printer.js
@@ -1,5 +1,5 @@
 function solve(day) {
-    let calendar = {
+        const calendar = {
         1: 'January',
         2: 'February',
         3: 'March',
@@ -13,13 +13,33 @@ function solve(day) {
         11: 'November',
         12: 'December',
     }
-    if (day < 1 || day > 12) {
-        console.log('Error!')
-    }else {
-        console.log(calendar[day])
-    }
-
+    console.log(calendar.hasOwnProperty(day) ? calendar[day]: 'Error!')
 }
+
+
+
+// function solve(day) {
+//     let calendar = {
+//         1: 'January',
+//         2: 'February',
+//         3: 'March',
+//         4: 'April',
+//         5: 'May',
+//         6: 'June',
+//         7: 'July',
+//         8: 'August',
+//         9: 'September',
+//         10: 'October',
+//         11: 'November',
+//         12: 'December',
+//     }
+//     if (day < 1 || day > 12) {
+//         console.log('Error!')
+//     }else {
+//         console.log(calendar[day])
+//     }
+//
+// }
 
 solve(12);
 
