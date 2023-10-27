6fa023f99f645478b2f046fa30433771aa4064f0
ceo-py <sfmf@abv.bg>
2023-10-27 16:46:53 +0300

    added new solution

--- Functions and Statements - Lab/6_sign_check.js
+++ Functions and Statements - Lab/6_sign_check.js
@@ -1,17 +1,22 @@
-
-function solve(...numbers){
-    let negativeNumbers = 0
-    for (const num of numbers){
-        if (num < 0){
-            negativeNumbers += 1
-        }
-    }
-    if (negativeNumbers === 0 || negativeNumbers === 2){
-        console.log('Positive')
-    } else {
-        console.log('Negative')
-    }
+function solve(...numbers) {
+    console.log(numbers.filter(n => n < 0).length % 2 === 0? 'Positive': 'Negative')
 }
+
+
+
+// function solve(...numbers){
+//     let negativeNumbers = 0
+//     for (const num of numbers){
+//         if (num < 0){
+//             negativeNumbers += 1
+//         }
+//     }
+//     if (negativeNumbers === 0 || negativeNumbers === 2){
+//         console.log('Positive')
+//     } else {
+//         console.log('Negative')
+//     }
+// }
 
 solve(-6,
 -12,
