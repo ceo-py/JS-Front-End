c3ad326786e78921ca5d01f0a39291ceef3e33b4
ceo-py <sfmf@abv.bg>
2023-10-27 18:16:12 +0300

    added new solution

--- Objects and Classes - Lab/4_convert_to_object.js
+++ Objects and Classes - Lab/4_convert_to_object.js
@@ -1,8 +1,15 @@
 function solve(string) {
-    string = JSON.parse(string);
-    for (const [key, value] of Object.entries(string)) {
-        console.log(`${key}: ${value}`)
-    }
+    const objectGen = JSON.parse(string)
+    Object.entries(objectGen).forEach(data => console.log(`${data[0]}: ${data[1]}`))
 }
+
+
+
+// function solve(string) {
+//     string = JSON.parse(string);
+//     for (const [key, value] of Object.entries(string)) {
+//         console.log(`${key}: ${value}`)
+//     }
+// }
 
 solve('{"name": "George", "age": 40, "town": "Sofia"}')
