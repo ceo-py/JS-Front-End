d6d53f02f0225a671ba05bf076ffbec3e1d65f27
ceo-py <sfmf@abv.bg>
2023-10-27 14:55:51 +0300

    added new solution

--- JS Syntax Fundamentals - Lab/5_math_operations.js
+++ JS Syntax Fundamentals - Lab/5_math_operations.js
@@ -1,13 +1,20 @@
 function solve (numOne, numTwo, operator) {
-    let result = {
-        '*': numOne * numTwo,
-        '/': numOne / numTwo,
-        '**': numOne ** numTwo,
-        '+': numOne + numTwo,
-        '-': numOne - numTwo,
-        '%': numOne % numTwo,
-    }
-    console.log(result[operator])
+
+    console.log(eval(`${numOne} ${operator} ${numTwo}`))
 }
+
+
+
+// function solve (numOne, numTwo, operator) {
+//     const result = {
+//         '*': numOne * numTwo,
+//         '/': numOne / numTwo,
+//         '**': numOne ** numTwo,
+//         '+': numOne + numTwo,
+//         '-': numOne - numTwo,
+//         '%': numOne % numTwo,
+//     }
+//     console.log(result[operator])
+// }
 
 solve(3, 5.5, '*')
