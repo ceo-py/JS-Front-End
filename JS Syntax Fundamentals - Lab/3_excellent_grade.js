e6bb599913eda3d553d38998e18d25cdb3898452
ceo-py <sfmf@abv.bg>
2023-10-27 14:16:38 +0300

    added new solution

--- JS Syntax Fundamentals - Lab/3_excellent_grade.js
+++ JS Syntax Fundamentals - Lab/3_excellent_grade.js
@@ -1,11 +1,17 @@
 function solve(num) {
-    if (num >= 5.50) {
-        console.log('Excellent');
-    } else {
-        console.log('Not excellent');
-    }
-
+    console.log(num >= 5.50 ? 'Excellent': 'Not excellent')
 }
+
+
+
+// function solve(num) {
+//     if (num >= 5.50) {
+//         console.log('Excellent');
+//     } else {
+//         console.log('Not excellent');
+//     }
+// }
+
 
 solve(5.50);
 
