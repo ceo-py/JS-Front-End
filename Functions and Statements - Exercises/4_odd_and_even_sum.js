f3c9606a06be638f2b5b482db41d3a593e089507
ceo-py <sfmf@abv.bg>
2023-10-27 16:51:50 +0300

    added new solution

--- Functions and Statements - Exercises/4_odd_and_even_sum.js
+++ Functions and Statements - Exercises/4_odd_and_even_sum.js
@@ -1,19 +1,27 @@
-
 function solve(numbers){
-    numbers = numbers.toString()
     let oddNumbers = 0
-    let evenNumbers = 0
-
-    for (let number of numbers) {
-        number = parseInt(number)
-        if (number % 2 === 0){
-            evenNumbers += number
-        } else {
-            oddNumbers += number
-        }
-    }
+    let evenNumbers = 0;
+    [...numbers.toString()].map(Number).forEach(n => n % 2 === 0? evenNumbers += n: oddNumbers += n)
     console.log(`Odd sum = ${oddNumbers}, Even sum = ${evenNumbers}`)
 }
+
+
+
+// function solve(numbers){
+//     numbers = numbers.toString()
+//     let oddNumbers = 0
+//     let evenNumbers = 0
+//
+//     for (let number of numbers) {
+//         number = parseInt(number)
+//         if (number % 2 === 0){
+//             evenNumbers += number
+//         } else {
+//             oddNumbers += number
+//         }
+//     }
+//     console.log(`Odd sum = ${oddNumbers}, Even sum = ${evenNumbers}`)
+// }
 
 
 solve(3495892137259234)
