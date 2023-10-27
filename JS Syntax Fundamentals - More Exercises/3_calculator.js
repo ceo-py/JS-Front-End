0e41a78f17acf8f77967db656df30b4dc12d2082
ceo-py <sfmf@abv.bg>
2023-10-27 15:51:52 +0300

    added new solution

--- JS Syntax Fundamentals - More Exercises/3_calculator.js
+++ JS Syntax Fundamentals - More Exercises/3_calculator.js
@@ -1,12 +1,17 @@
 function solve (numOne, operator, numTwo) {
-    let result = {
-        '*': numOne * numTwo,
-        '/': numOne / numTwo,
-        '+': numOne + numTwo,
-        '-': numOne - numTwo,
-    }
-    console.log(result[operator].toFixed(2))
+    console.log(eval(`${numOne}${operator}${numTwo}`).toFixed(2))
 }
+
+
+// function solve (numOne, operator, numTwo) {
+//     let result = {
+//         '*': numOne * numTwo,
+//         '/': numOne / numTwo,
+//         '+': numOne + numTwo,
+//         '-': numOne - numTwo,
+//     }
+//     console.log(result[operator].toFixed(2))
+// }
 
 solve(5,
 '+',
