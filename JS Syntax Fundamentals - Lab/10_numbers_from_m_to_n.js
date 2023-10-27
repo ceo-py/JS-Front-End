3e74a59c653a517f0b250e126245647c378673b1
ceo-py <sfmf@abv.bg>
2023-10-27 15:09:02 +0300

    added new solution

--- JS Syntax Fundamentals - Lab/10_numbers_from_m_to_n.js
+++ JS Syntax Fundamentals - Lab/10_numbers_from_m_to_n.js
@@ -1,8 +1,15 @@
-function solve (start, end) {
-
-    for (let i=start; i>=end; i--){
-        console.log(i)
+function solve(start, end) {
+    while (start >= end) {
+        console.log(start)
+        start -= 1
     }
 }
+
+// function solve (start, end) {
+//
+//     for (let i=start; i>=end; i--){
+//         console.log(i)
+//     }
+// }
 
 solve(6, 2)
