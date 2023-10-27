c7e389cff2948aa72af3b23468b51e7d4928dea0
ceo-py <sfmf@abv.bg>
2023-10-27 15:24:42 +0300

    added new solution

--- JS Syntax Fundamentals - Exercises/6_sum_digits.js
+++ JS Syntax Fundamentals - Exercises/6_sum_digits.js
@@ -1,13 +1,17 @@
-
 function solve(number) {
-    let numbers = number.toString();
-    let sum = 0
-
-    for (let i=0; i < numbers.length; i++){
-        sum += parseInt(numbers[i])
-    }
-    console.log(sum)
+    console.log(eval(Array.from(number.toString()).join('+')))
 }
+
+
+// function solve(number) {
+//     let numbers = number.toString();
+//     let sum = 0
+//
+//     for (let i=0; i < numbers.length; i++){
+//         sum += parseInt(numbers[i])
+//     }
+//     console.log(sum)
+// }
 
 
 solve(245678)
