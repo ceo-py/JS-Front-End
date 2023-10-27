869c0083c1ddd31cac4bc81d7ab8d600e1195c73
ceo-py <sfmf@abv.bg>
2023-10-27 15:19:23 +0300

    added new solution

--- JS Syntax Fundamentals - Exercises/4_print_and_sum.js
+++ JS Syntax Fundamentals - Exercises/4_print_and_sum.js
@@ -1,9 +1,19 @@
 function solve(start, end) {
-    let sum = [];
-    for (let i = start; i <= end; i++) {
-        sum.push(i)
+    const sum = []
+    while (start <= end) {
+        sum.push(start)
+        start += 1
     }
-    console.log(`${sum.join(' ')}\nSum: ${sum.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`);
+    sum.forEach(x => process.stdout.write(`${x} `))
+    console.log(`\nSum: ${eval(sum.join('+'))}`)
 }
+
+// function solve(start, end) {
+//     const sum = []
+//     for (let i = start; i <= end; i++) {
+//         sum.push(i)
+//     }
+//     console.log(`${sum.join(' ')}\nSum: ${sum.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`)
+// }
 
 solve(5, 10)
