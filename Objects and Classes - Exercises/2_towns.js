29b6884481bec9d070fc1ff32192a88def37b575
ceo-py <sfmf@abv.bg>
2023-10-27 19:14:48 +0300

    added new solution

--- Objects and Classes - Exercises/2_towns.js
+++ Objects and Classes - Exercises/2_towns.js
@@ -1,21 +1,30 @@
-
-function solve(list){
-    let output = []
-
-    for (let i=0; i < list.length; i++){
-        let [name, lat, lon] = list[i].split(' | ')
-
-        const town = {
-            town: name,
-            latitude: parseFloat(lat).toFixed(2),
-            longitude: parseFloat(lon).toFixed(2),
+function solve(list) {
+    const town = (town, latitude, longitude) => {
+        return {
+            town, latitude, longitude
         }
-        output.push(town)
     }
-    for (let data of output){
-        console.log(data)
-    }
+    list.map(x => console.log(town(...x.split(' | ').map(d => isNaN(d)? d : parseFloat(d).toFixed(2)))))
 }
+
+
+// function solve(list){
+//     let output = []
+//
+//     for (let i=0; i < list.length; i++){
+//         let [name, lat, lon] = list[i].split(' | ')
+//
+//         const town = {
+//             town: name,
+//             latitude: parseFloat(lat).toFixed(2),
+//             longitude: parseFloat(lon).toFixed(2),
+//         }
+//         output.push(town)
+//     }
+//     for (let data of output){
+//         console.log(data)
+//     }
+// }
 
 
 solve(['Sofia | 42.696552 | 23.32601',
