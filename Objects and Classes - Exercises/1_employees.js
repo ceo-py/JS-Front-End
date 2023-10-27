829252c3ec14be1d6e3e51418d584e470e4d7a75
ceo-py <sfmf@abv.bg>
2023-10-27 19:09:29 +0300

    added new solution

--- Objects and Classes - Exercises/1_employees.js
+++ Objects and Classes - Exercises/1_employees.js
@@ -1,19 +1,31 @@
 function solve(list) {
-    let output = []
-
-    for (let item of list) {
-        const employee = {
-            name: item,
-            personalNumber: item.length
+    const employee = (name, personalNumber) => {
+        return {
+            name, personalNumber, showData: () => {
+                return `Name: ${name} -- Personal Number: ${personalNumber}`
+            }
         }
-        output.push(employee)
-
     }
-
-    for (let item of output) {
-        console.log(`Name: ${item.name} -- Personal Number: ${item.personalNumber}`)
-    }
+    list.map(x => console.log(employee(x, x.length).showData()))
 }
+
+
+// function solve(list) {
+//     let output = []
+//
+//     for (let item of list) {
+//         const employee = {
+//             name: item,
+//             personalNumber: item.length
+//         }
+//         output.push(employee)
+//
+//     }
+//
+//     for (let item of output) {
+//         console.log(`Name: ${item.name} -- Personal Number: ${item.personalNumber}`)
+//     }
+// }
 
 
 solve([
