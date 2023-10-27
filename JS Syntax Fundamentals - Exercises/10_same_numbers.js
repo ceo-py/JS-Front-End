5fa69773b3a3a40ad58446b8aa5c9c1fd99a969b
ceo-py <sfmf@abv.bg>
2023-10-27 15:41:54 +0300

    added new solution

--- JS Syntax Fundamentals - Exercises/10_same_numbers.js
+++ JS Syntax Fundamentals - Exercises/10_same_numbers.js
@@ -1,21 +1,28 @@
-function solve(number) {
-    number = number.toString()
-    let numberIs = number[0]
-    let equalNumbers = ""
-    let sumNumbers = 0
-
-    for (let i = 0; i < number.length; i++) {
-        if (numberIs === number[i]){
-            equalNumbers += "1"
-        }
-        sumNumbers += parseInt(number[i])
-    }
-    if (equalNumbers.length === number.length){
-        console.log(`true\n${sumNumbers}`)
-    } else {
-        console.log(`false\n${sumNumbers}`)
-    }
+function solve(numbers) {
+    const strNumbers = Array.from(numbers.toString())
+    const sum = eval(strNumbers.join('+'))
+    console.log(`${Array.from(strNumbers.filter((x) => x === strNumbers[0])).length === strNumbers.length}\n${sum}`)
 }
 
 
-solve(1234)
+// function solve(number) {
+//     number = number.toString()
+//     let numberIs = number[0]
+//     let equalNumbers = ""
+//     let sumNumbers = 0
+//
+//     for (let i = 0; i < number.length; i++) {
+//         if (numberIs === number[i]){
+//             equalNumbers += "1"
+//         }
+//         sumNumbers += parseInt(number[i])
+//     }
+//     if (equalNumbers.length === number.length){
+//         console.log(`true\n${sumNumbers}`)
+//     } else {
+//         console.log(`false\n${sumNumbers}`)
+//     }
+// }
+
+
+solve(2222222)
