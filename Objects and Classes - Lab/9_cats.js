f85af11ecc9f467fd119bd44083da510935b150e
ceo-py <sfmf@abv.bg>
2023-10-27 18:53:25 +0300

    added new solution

--- Objects and Classes - Lab/9_cats.js
+++ Objects and Classes - Lab/9_cats.js
@@ -1,19 +1,37 @@
 function solve(list) {
-    class Cat {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        meow() {
-            console.log(`${this.name}, age ${this.age} says Meow`)
+    const cat = (name, age) => {
+        return {
+            name,
+            age,
+            meow: () => {
+                return `${name}, age ${age} says Meow`
+            }
         }
     }
-    for (let item of list){
-        let [name, age] = item.split(' ')
-        let catObj = new Cat(name, age)
-        catObj.meow()
-    }
+    list.forEach(x => {
+        console.log(cat(...x.split(' ')).meow())
+    })
 }
+
+
+
+
+// function solve(list) {
+//     class Cat {
+//         constructor(name, age) {
+//             this.name = name;
+//             this.age = age;
+//         }
+//
+//         meow() {
+//             console.log(`${this.name}, age ${this.age} says Meow`)
+//         }
+//     }
+//     for (let item of list){
+//         let [name, age] = item.split(' ')
+//         let catObj = new Cat(name, age)
+//         catObj.meow()
+//     }
+// }
 
 solve(['Mellow 2', 'Tom 5'])
