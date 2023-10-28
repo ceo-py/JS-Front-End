93a90f81f870b3ea2e5779fe3b04e6a7996d8829
ceo-py <sfmf@abv.bg>
2023-10-28 21:36:44 +0300

    added new solution

--- HTTP and REST - Exercises/05. Messenger/5_messenger.js
+++ HTTP and REST - Exercises/05. Messenger/5_messenger.js
@@ -1,35 +1,81 @@
 function attachEvents() {
-    const name = document.querySelector('input[name=\'author\']')
-    const content = document.querySelector('input[name=\'content\']')
+    const messagesAPI = 'http://localhost:3030/jsonstore/messenger'
+    const author = document.querySelector('input[name="author"]')
+    const content = document.querySelector('input[name="content"]')
     const messages = document.querySelector('#messages')
-    const [submit, refresh] = Array.from(document.querySelectorAll('#controls > input'))
-    const apiUrl = 'http://localhost:3030/jsonstore/messenger'
 
-    submit.addEventListener('click', () => {
 
-        const message = {
-            author: name.value,
-            content: content.value
-        }
-
-        fetch(apiUrl, {
+    const submit = async () => {
+        await fetch(messagesAPI, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(message),
+            body: JSON.stringify({
+                author: author.value,
+                content: content.value,
+            })
         })
-    })
+        author.value = ''
+        content.value = ''
+        await refresh()
+    }
 
-    refresh.addEventListener('click', () => {
-        messages.textContent = ''
-        let output = []
-        fetch(apiUrl).then(x => x.json()).then(x => {
-            for (const key in x) {
-                output.push(`${x[key].author}: ${x[key].content}`)
-            }
-            messages.textContent = output.join('\n')
-        }).catch()
-    })
+    const refresh = async () => {
+        const response = await fetch(messagesAPI)
+        const dataMessages = await response.json()
+        const output = []
+        Object.keys(dataMessages).forEach(x => {
+            const currentElement = dataMessages[x]
+            output.push(`${currentElement.author}: ${currentElement.content}`)
+        })
+        messages.textContent = output.join('\n')
+    }
+
+    return {
+        submit,
+        refresh
+    }
 }
 
-attachEvents();
+const funcJS = attachEvents();
+
+
+
+
+
+
+
+// function attachEvents() {
+//     const name = document.querySelector('input[name=\'author\']')
+//     const content = document.querySelector('input[name=\'content\']')
+//     const messages = document.querySelector('#messages')
+//     const [submit, refresh] = Array.from(document.querySelectorAll('#controls > input'))
+//     const apiUrl = 'http://localhost:3030/jsonstore/messenger'
+//
+//     submit.addEventListener('click', () => {
+//
+//         const message = {
+//             author: name.value,
+//             content: content.value
+//         }
+//
+//         fetch(apiUrl, {
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify(message),
+//         })
+//     })
+//
+//     refresh.addEventListener('click', () => {
+//         messages.textContent = ''
+//         let output = []
+//         fetch(apiUrl).then(x => x.json()).then(x => {
+//             for (const key in x) {
+//                 output.push(`${x[key].author}: ${x[key].content}`)
+//             }
+//             messages.textContent = output.join('\n')
+//         }).catch()
+//     })
+// }
+//
+// attachEvents();
 
