cd090af1b2038ce1e034fa0b61c4d615cfcd9cd3
ceo-py <sfmf@abv.bg>
2023-10-28 21:30:25 +0300

    added new solution

--- HTTP and REST - Exercises/01. Bus Stop/1_bus_stop.js
+++ HTTP and REST - Exercises/01. Bus Stop/1_bus_stop.js
@@ -53,3 +53,54 @@ function getInfo() {
 //     }
 // }
 
+
+
+//
+// async function getInfo() {
+//
+//     const stopId = document.querySelector('#stopId').value
+//     const stopName = document.querySelector('#stopName')
+//     const buses = document.querySelector('#buses')
+//     const API = 'http://localhost:3030/jsonstore/bus/businfo/'
+//
+//     const test = {
+//         "buses": {"76": 15, "84": 10, "204": 10, "213": 18, "280": 9, "306": 31, "604": 11},
+//         "name": "Orlov Most sq."
+//     }
+//
+//     // fetch(`${API}${stopId}`)
+//     //     .then(x => x.json())
+//     //     .then(x => {
+//     //         stopName.textContent = x.name
+//     //         // x['buses']
+//     //         buses.innerHTML = ''
+//     //         for (const bus in x.buses) {
+//     //             const li = document.createElement('li')
+//     //             li.textContent = `Bus ${bus} arrives in ${x.buses[bus]} minutes`
+//     //             buses.appendChild(li)
+//     //         }
+//     //
+//     //     })
+//     //     .catch(() => {
+//     //         stopName.textContent = 'Error'
+//     //     })
+//
+//
+//     try {
+//         const response = await fetch(`${API}${stopId}`)
+//         const x = await response.json()
+//         stopName.textContent = x.name
+//         // x['buses']
+//         buses.innerHTML = ''
+//         for (const bus in x.buses) {
+//             const li = document.createElement('li')
+//             li.textContent = `Bus ${bus} arrives in ${x.buses[bus]} minutes`
+//             buses.appendChild(li)
+//         }
+//     } catch (err) {
+//         console.log(err)
+//         stopName.textContent = 'Error'
+//     }
+//
+// }
+
