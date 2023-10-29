5c734b21413558d8cb1a480a0675721d6a2be262
ceo-py <sfmf@abv.bg>
2023-10-30 00:33:46 +0200

    added new solution

--- DOM and Events - Exercises/05. Fill Dropdown/05_fill_dropdown.js
+++ DOM and Events - Exercises/05. Fill Dropdown/05_fill_dropdown.js
@@ -1,14 +1,26 @@
 function addItem() {
     const newItemText = document.querySelector('#newItemText')
     const newItemValue = document.querySelector('#newItemValue')
-    const menu = document.querySelector('#menu')
-
-    const option = document.createElement('option')
-    option.textContent = newItemText.value
-    option.value = newItemValue.value
-    menu.appendChild(option)
+    document.querySelector('#menu').innerHTML += `<option value="${newItemValue.value}">${newItemText.value}</option>`
     newItemText.value = ''
     newItemValue.value = ''
 }
+
+
+
+
+
+// function addItem() {
+//     const newItemText = document.querySelector('#newItemText')
+//     const newItemValue = document.querySelector('#newItemValue')
+//     const menu = document.querySelector('#menu')
+//
+//     const option = document.createElement('option')
+//     option.textContent = newItemText.value
+//     option.value = newItemValue.value
+//     menu.appendChild(option)
+//     newItemText.value = ''
+//     newItemValue.value = ''
+// }
 
 
