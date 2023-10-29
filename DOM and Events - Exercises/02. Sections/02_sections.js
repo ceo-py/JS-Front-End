5802c5c7d72900c5cf3a976a628233f886cecabe
ceo-py <sfmf@abv.bg>
2023-10-30 00:27:40 +0200

    added new solution

--- DOM and Events - Exercises/02. Sections/02_sections.js
+++ DOM and Events - Exercises/02. Sections/02_sections.js
@@ -1,14 +1,22 @@
 function create(list) {
-    const content = document.getElementById("content");
-    for (let i = 0; i < list.length; i++) {
-        const div = document.createElement('div')
-        const p = document.createElement("p")
-        p.textContent = list[i]
-        p.style.display = "none"
-        div.appendChild(p)
-        div.addEventListener("click", () => {
-            p.style.display = "block"
-        })
-        content.appendChild(div)
-    }
+    list.forEach(x => document.querySelector("#content").innerHTML += `<div><p style="display: none">${x}</p></div>`);
+    [...document.querySelectorAll("div div")].forEach(x => x.addEventListener('click', () => x.querySelector('p').style.display = "block"))
 }
+
+
+
+
+// function create(list) {
+//     const content = document.getElementById("content");
+//     for (let i = 0; i < list.length; i++) {
+//         const div = document.createElement('div')
+//         const p = document.createElement("p")
+//         p.textContent = list[i]
+//         p.style.display = "none"
+//         div.appendChild(p)
+//         div.addEventListener("click", () => {
+//             p.style.display = "block"
+//         })
+//         content.appendChild(div)
+//     }
+// }
