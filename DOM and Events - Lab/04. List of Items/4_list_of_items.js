2b185af18d7c9b450fde989008daa167278835ef
ceo-py <sfmf@abv.bg>
2023-10-29 22:34:09 +0200

    added new solution

--- DOM and Events - Lab/04. List of Items/4_list_of_items.js
+++ DOM and Events - Lab/04. List of Items/4_list_of_items.js
@@ -1,12 +1,20 @@
 function addItem() {
-  const newItemText = document.getElementById("newItemText").value.trim();
-  
-  if (newItemText !== "") {
-    const newItem = document.createElement("li");
-    newItem.textContent = newItemText;
-    
-    document.getElementById("items").appendChild(newItem);
-    
-    document.getElementById("newItemText").value = "";
-  }
+    const newItemText = document.querySelector("#newItemText").value.trim()
+    console.log(newItemText)
+    document.querySelector("#items").innerHTML += `<li>${newItemText}</li>`
+    document.querySelector("#newItemText").value = ""
 }
+
+
+// function addItem() {
+//   const newItemText = document.getElementById("newItemText").value.trim();
+//  
+//   if (newItemText !== "") {
+//     const newItem = document.createElement("li");
+//     newItem.textContent = newItemText;
+//    
+//     document.getElementById("items").appendChild(newItem);
+//    
+//     document.getElementById("newItemText").value = "";
+//   }
+// }
