c498d5c02f0fb3b418b9b84804b88911cbeaad1c
ceo-py <sfmf@abv.bg>
2023-10-29 19:18:11 +0200

    added new solution

--- DOM and Events - Lab/08. Sum Table/08_sum_table.js
+++ DOM and Events - Lab/08. Sum Table/08_sum_table.js
@@ -1,15 +1,20 @@
 function sumTable() {
-    const table = document.querySelectorAll('td')
-    let sum = document.getElementById('sum')
-
-    let total_sum = 0
-
-    for (let i = 0; i < table.length - 1; i++) {
-        if (i % 2 !== 0) {
-            total_sum += parseFloat(table[i].innerText)
-        }
-    }
-    sum.innerText = total_sum.toFixed(2)
-
+    document.querySelector('#sum').textContent = ([...document.querySelectorAll('td')].slice(0, -2).filter((_, i) => i % 2 !== 0)
+        .reduce((sum, x) => sum + parseFloat(x.textContent), 0)).toFixed(2)
 }
+
+// function sumTable() {
+//     const table = document.querySelectorAll('td')
+//     let sum = document.getElementById('sum')
+//
+//     let total_sum = 0
+//
+//     for (let i = 0; i < table.length - 1; i++) {
+//         if (i % 2 !== 0) {
+//             total_sum += parseFloat(table[i].innerText)
+//         }
+//     }
+//     sum.innerText = total_sum.toFixed(2)
+//
+// }
 
