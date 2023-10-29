ee708f00180fdf34ef62c8eb35cc0e24b0877e2d
ceo-py <sfmf@abv.bg>
2023-10-29 21:13:16 +0200

    added new solution

--- DOM and Events - Exercises/01. Subtraction/01_subtraction.js
+++ DOM and Events - Exercises/01. Subtraction/01_subtraction.js
@@ -1,5 +1,15 @@
 function subtract() {
-    let numberOne = parseFloat(document.querySelector('#firstNumber').value)
-    let numberTwo = parseFloat(document.querySelector('#secondNumber').value)
-    document.querySelector('#result').textContent = numberOne - numberTwo
+    document.querySelector('#result').textContent = parseFloat(document.querySelector('#firstNumber').value) - parseFloat(document.querySelector('#secondNumber').value);
+
 }
+
+
+
+
+// function subtract() {
+//     let numberOne = parseFloat(document.querySelector('#firstNumber').value)
+//     let numberTwo = parseFloat(document.querySelector('#secondNumber').value)
+//     document.querySelector('#result').textContent = numberOne - numberTwo
+// }
+
+subtract
