e250afdf7a2f57dbec40b839e99072237e3acb8b
ceo-py <sfmf@abv.bg>
2023-10-29 19:07:44 +0200

    added new solution

--- DOM and Events - Lab/07. Colorize Table/07_colorize_table.js
+++ DOM and Events - Lab/07. Colorize Table/07_colorize_table.js
@@ -1,9 +1,14 @@
 function colorize() {
-    const table = document.querySelector('table').rows
-    for (i = 0; i < table.length; i++) {
-        if (i % 2 !== 0) {
-            table[i].style.backgroundColor = 'Teal'
-        }
-    }
+    [...document.querySelectorAll('tr')].map((x , i) => i % 2 !== 0 ? x.style.backgroundColor = 'Teal': '')
 }
+
+
+// function colorize() {
+//     const table = document.querySelector('table').rows
+//     for (i = 0; i < table.length; i++) {
+//         if (i % 2 !== 0) {
+//             table[i].style.backgroundColor = 'Teal'
+//         }
+//     }
+// }
     
