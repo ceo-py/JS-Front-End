392948741e7089ecf89cae42b36bb114c291ebec
ceo-py <sfmf@abv.bg>
2023-10-29 23:34:57 +0200

    added new solution

--- DOM and Events - More Exercises/03. Mouse Gradient/3_mouse_gradient.js
+++ DOM and Events - More Exercises/03. Mouse Gradient/3_mouse_gradient.js
@@ -1,18 +1,30 @@
 function attachGradientEvents() {
     const gradient = document.querySelector('#gradient')
     const result = document.querySelector('#result')
-    gradient.addEventListener('mouseout', hidePercent)
-    gradient.addEventListener('mousemove', showPercent)
-
-
-    function showPercent(event) {
-        const x = event.offsetX
-        const width = gradient.clientWidth
-        const percentage = parseInt((x / width) * 100)
-        result.textContent = `${percentage}%`
-    }
-
-    function hidePercent() {
-        result.textContent = ''
-    }
+    gradient.addEventListener('mousemove', (e) => result.textContent = `${parseInt((e.offsetX / gradient.clientWidth) * 100)}%`)
+    gradient.addEventListener('mouseout', () => result.textContent = '')
 }
+
+
+
+
+
+
+// function attachGradientEvents() {
+//     const gradient = document.querySelector('#gradient')
+//     const result = document.querySelector('#result')
+//     gradient.addEventListener('mouseout', hidePercent)
+//     gradient.addEventListener('mousemove', showPercent)
+//
+//
+//     function showPercent(event) {
+//         const x = event.offsetX
+//         const width = gradient.clientWidth
+//         const percentage = parseInt((x / width) * 100)
+//         result.textContent = `${percentage}%`
+//     }
+//
+//     function hidePercent() {
+//         result.textContent = ''
+//     }
+// }
