54cc2066df018960d84e59747b12022f6fd80bea
ceo-py <sfmf@abv.bg>
2023-10-29 23:01:22 +0200

    added new solution

--- DOM and Events - Lab/05. Delete from Table/5_delete_from_table.js
+++ DOM and Events - Lab/05. Delete from Table/5_delete_from_table.js
@@ -1,17 +1,31 @@
 function deleteByEmail() {
-    const email = document.getElementsByName("email")[0].value
-    const table = document.getElementById("customers")
-    let text = "Not found."
-
-    for (let i = 1; i < table.rows.length; i++) {
-        const row = table.rows[i]
-        if (row.cells[1].textContent === email) {
-            table.deleteRow(i)
-            text = "Deleted."
-            break
-        }
-    }
-    document.getElementById("result").textContent = "Not found."
-    document.getElementsByName("email")[0].value = ""
+    const table = [...document.querySelectorAll('tbody tr')]
+    const email = document.querySelector('input[type="text"]')
+    const result = document.querySelector('#result')
+    result.textContent = 'Not found.'
+    table.forEach(x => [...x.querySelectorAll('td')][1].textContent === email.value? [x.remove(),result.textContent = 'Deleted.']: null)
+    email.value = ''
 }
+
+
+
+
+
+
+// function deleteByEmail() {
+//     const email = document.getElementsByName("email")[0].value
+//     const table = document.getElementById("customers")
+//     let text = "Not found."
+//
+//     for (let i = 1; i < table.rows.length; i++) {
+//         const row = table.rows[i]
+//         if (row.cells[1].textContent === email) {
+//             table.deleteRow(i)
+//             text = "Deleted."
+//             break
+//         }
+//     }
+//     document.getElementById("result").textContent = text
+//     document.getElementsByName("email")[0].value = ""
+// }
 
