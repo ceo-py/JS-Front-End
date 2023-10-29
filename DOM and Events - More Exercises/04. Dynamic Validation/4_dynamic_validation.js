179dc3f5ff8d2262bc2f01413644f02991e61aa6
ceo-py <sfmf@abv.bg>
2023-10-29 23:43:07 +0200

    added new solution

--- DOM and Events - More Exercises/04. Dynamic Validation/4_dynamic_validation.js
+++ DOM and Events - More Exercises/04. Dynamic Validation/4_dynamic_validation.js
@@ -1,13 +1,25 @@
 function validate() {
     const email = document.querySelector('#email')
-
-    function validateEmail() {
-        const output = email.value.match(/^[a-z]+@[a-z]+\.[a-z]+$/)
-        if (!output) {
-            email.classList.add('error')
-        } else {
-            email.classList.remove('error')
-        }
-    }
-    email.addEventListener('change', validateEmail)
+    email.addEventListener('change', () => 
+        email.value.match(/^[a-z]+@[a-z]+\.[a-z]+$/)? email.classList.remove('error'): email.classList.add('error'))
 }
+
+
+
+
+
+
+
+// function validate() {
+//     const email = document.querySelector('#email')
+//
+//     function validateEmail() {
+//         const output = email.value.match(/^[a-z]+@[a-z]+\.[a-z]+$/)
+//         if (!output) {
+//             email.classList.add('error')
+//         } else {
+//             email.classList.remove('error')
+//         }
+//     }
+//     email.addEventListener('change', validateEmail)
+// }
