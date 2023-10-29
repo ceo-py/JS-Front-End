79212b17c7b9d276c08f57de410fde5961287a35
ceo-py <sfmf@abv.bg>
2023-10-29 19:03:42 +0200

    added new solution

--- DOM and Events - Lab/03. Collect List Items/3_collect_list_Items.js
+++ DOM and Events - Lab/03. Collect List Items/3_collect_list_Items.js
@@ -1,15 +1,21 @@
 function extractText() {
-    const liElements = document.getElementsByTagName("li")
-
-    let text = "";
-
-    for (let i = 0; i < liElements.length; i++) {
-        text += liElements[i].innerHTML + "\n";
-    }
-
-    const resultTextArea = document.getElementById("result");
-    resultTextArea.value = text;
-
+    document.querySelector('textarea').textContent = [...document.querySelectorAll('li')]
+                                                            .map(x => x.textContent).join('\n')
 }
+
+
+// function extractText() {
+//     const liElements = document.getElementsByTagName("li")
+//
+//     let text = "";
+//
+//     for (let i = 0; i < liElements.length; i++) {
+//         text += liElements[i].innerHTML + "\n";
+//     }
+//
+//     const resultTextArea = document.getElementById("result");
+//     resultTextArea.value = text;
+//
+// }
 
 
