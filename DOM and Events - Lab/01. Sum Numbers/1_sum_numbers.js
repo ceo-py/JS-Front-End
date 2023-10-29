fc76e22f8bf1ebcfdc0160a1def4082fe8742fad
ceo-py <sfmf@abv.bg>
2023-10-29 18:57:26 +0200

    added new solution

--- DOM and Events - Lab/01. Sum Numbers/1_sum_numbers.js
+++ DOM and Events - Lab/01. Sum Numbers/1_sum_numbers.js
@@ -1,7 +1,12 @@
 function calc() {
-    const num1 = parseFloat(document.getElementById('num1').value)
-    const num2 = parseFloat(document.getElementById('num2').value)
-    const sum = num1 + num2
-    document.getElementById('sum').value = sum
-    return sum
+    document.querySelector('#sum').value = eval([...document.querySelectorAll('input[type="text"]')]
+        .slice(0,2).map(x => parseFloat(x.value)).join('+'))
 }
+
+
+// function calc() {
+//     const num1 = parseFloat(document.getElementById('num1').value)
+//     const num2 = parseFloat(document.getElementById('num2').value)
+//     const sum = num1 + num2
+//     document.getElementById('sum').value = sum
+// }
