d2ef281fa8354f5b920979ff41df2b07123fb6a7
ceo-py <sfmf@abv.bg>
2023-10-29 23:40:19 +0200

    added new solution

--- DOM and Events - Lab/09. Highlight Active/08_highligh_active.js
+++ DOM and Events - Lab/09. Highlight Active/08_highligh_active.js
@@ -1,14 +1,24 @@
 function focused() {
-    const inputs = document.querySelectorAll("input[type='text']")
-    for (let i = 0; i < inputs.length; i++) {
-        const input = inputs[i]
-        input.addEventListener('focus', (event) => {
-            const div = event.target.parentNode
-            div.classList.add('focused')
-        });
-        input.addEventListener('blur', (event) => {
-            const div = event.target.parentNode
-            div.classList.remove('focused')
+    [...document.querySelectorAll("input[type='text']")].forEach(x => {
+            x.addEventListener('focus', (e) => e.target.parentNode.classList.add('focused'))
+            x.addEventListener('blur', (e) => e.target.parentNode.classList.remove('focused'))
         })
-    }
 }
+
+
+
+
+// function focused() {
+//     const inputs = document.querySelectorAll("input[type='text']")
+//     for (let i = 0; i < inputs.length; i++) {
+//         const input = inputs[i]
+//         input.addEventListener('focus', (event) => {
+//             const div = event.target.parentNode
+//             div.classList.add('focused')
+//         });
+//         input.addEventListener('blur', (event) => {
+//             const div = event.target.parentNode
+//             div.classList.remove('focused')
+//         })
+//     }
+// }
