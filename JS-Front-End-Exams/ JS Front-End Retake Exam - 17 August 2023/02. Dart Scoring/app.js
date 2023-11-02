dc61213889dde63333c41562d5e8a9ef4faa3210
ceo-py <sfmf@abv.bg>
2023-11-02 15:07:36 +0200

    added new solution

---
+++ JS-Front-End-Exams/ JS Front-End Retake Exam - 17 August 2023/02. Dart Scoring/app.js
@@ -0,0 +1,62 @@
+window.addEventListener("load", solve);
+
+function solve() {
+    const dartScoring = {
+        inputFields: {
+            player: document.querySelector('#player'),
+            score: document.querySelector('#score'),
+            round: document.querySelector('#round'),
+        },
+        addBtn: document.querySelector('#add-btn'),
+        sureList: document.querySelector('#sure-list'),
+        scoreboardList: document.querySelector('#scoreboard-list'),
+        clearBtn: document.querySelector('.clear'),
+        inputData: [],
+    }
+
+    const correctInput = (input) => input.every(x => x.value.length !== 0)
+    const clearInputFields = (input) => input.forEach(x => x.value = '')
+
+    const editBtnFunc = (e) => {
+        dartScoring.sureList.removeChild(e.target.parentNode)
+        Object.values(dartScoring.inputFields).forEach((x, i) => x.value = dartScoring.inputData[i])
+        dartScoring.addBtn.disabled = false
+    }
+
+    const okBtnFunc = (e) => {
+        const currentContainer = e.target.parentNode
+        dartScoring.sureList.removeChild(currentContainer);
+        [...currentContainer.querySelectorAll('button')].forEach(x => x.remove())
+        dartScoring.scoreboardList.appendChild(currentContainer)
+        dartScoring.addBtn.disabled = false
+
+    }
+
+    const addBtnFunc = (e) => {
+        e.preventDefault()
+        if (!correctInput(Object.values(dartScoring.inputFields))) return
+        dartScoring.inputData = Object.values(dartScoring.inputFields).map(x => x.value)
+        dartScoring.sureList.innerHTML = `
+            <li class="dart-item">
+                <article>
+                    <p>${dartScoring.inputFields.player.value}</p>
+                    <p>Score: ${dartScoring.inputFields.score.value}</p>
+                    <p>Round: ${dartScoring.inputFields.round.value}</p>
+                </article>
+                <button class="btn edit">edit</button>
+                <button class="btn ok">ok</button>
+            </li>`;
+        [...dartScoring.sureList.querySelectorAll('button')].forEach((x, i) => x.addEventListener('click', i % 2 === 0 ? editBtnFunc : okBtnFunc))
+        dartScoring.addBtn.disabled = true
+        clearInputFields(Object.values(dartScoring.inputFields))
+    }
+
+    const clearBtnFunc = (e) => {
+        e.preventDefault()
+        dartScoring.scoreboardList.innerHTML = ''
+    }
+
+    dartScoring.addBtn.addEventListener('click', addBtnFunc)
+    dartScoring.clearBtn.addEventListener('click', clearBtnFunc)
+}
+  
