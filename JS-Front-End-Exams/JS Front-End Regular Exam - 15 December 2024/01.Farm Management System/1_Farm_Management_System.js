8788b48d22b73e82fbedf6711f7fd0c535a25654
ceo-py <sfmf@abv.bg>
2024-12-20 16:41:02 +0200

    add another exam solution JS Front-End Regular Exam - 15 December 2024 => 01. Farm Management System

---
+++ JS-Front-End-Exams/JS Front-End Regular Exam - 15 December 2024/01.Farm Management System/1_Farm_Management_System.js
@@ -0,0 +1,66 @@
+function farmManagementSystem(input) {
+  const n = Number(input.shift());
+  const [farmers, farmersToAdd, commandsToExecute] = [
+    {},
+    input.slice(0, n),
+    input.slice(n),
+  ];
+  const commands = {
+    Execute(name, workArea, task) {
+      const farmer = farmers[name];
+      return farmer.workArea === workArea && farmer.tasks.includes(task)
+        ? `${name} has executed the task: ${task}!`
+        : `${name} cannot execute the task: ${task}.`;
+    },
+    "Change Area"(name, newWorkArea) {
+      const farmer = farmers[name];
+      farmer.workArea = newWorkArea;
+      return `${name} has changed their work area to: ${newWorkArea}`;
+    },
+    "Learn Task"(name, newTask) {
+      const farmer = farmers[name];
+      if (farmer.tasks.includes(newTask)) {
+        return `${name} already knows how to perform ${newTask}.`;
+      }
+      farmer.tasks.push(newTask);
+      return `${name} has learned a new task: ${newTask}.`;
+    },
+  };
+
+  farmersToAdd.forEach((farmer) => {
+    const [name, workArea, tasks] = farmer
+      .split(" ")
+      .map((el, i) => (i === 2 ? el.split(",") : el));
+    farmers[name] = {
+      workArea,
+      tasks,
+    };
+  });
+  let commandString = commandsToExecute.shift();
+  while (commandString !== "End") {
+    const [command, ...args] = commandString.split(" / ");
+    console.log(commands[command](...args));
+    commandString = commandsToExecute.shift();
+  }
+
+  Object.entries(farmers).forEach(([name, details]) => {
+    console.log(
+      `Farmer: ${name}, Area: ${details.workArea}, Tasks: ${details.tasks
+        .sort()
+        .join(", ")}`
+    );
+  });
+}
+
+farmManagementSystem([
+  "2",
+  "John garden watering,weeding",
+  "Mary barn feeding,cleaning",
+  "Execute / John / garden / watering",
+  "Execute / Mary / garden / feeding",
+  "Learn Task / John / planting",
+  "Execute / John / garden / planting",
+  "Change Area / Mary / garden",
+  "Execute / Mary / garden / cleaning",
+  "End",
+]);
